40af8722112bb9b196cf3399bc5d96671c1be0bd
hard <harleydav@mail.ru>
2018-03-06 21:41:37 +0300

    threads

---
+++ java9-core/multithreading/threads/src/main/java/com/hard/_01_create_threads/_03_anonymous_class/_01_runnable/Main.java
@@ -0,0 +1,23 @@
+package com.hard._01_create_threads._03_anonymous_class._01_runnable;
+
+public class Main {
+    public static void main(String[] args) {
+        new Thread(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        System.out.println(Thread.currentThread().getName() + ": Hello World");
+                    }
+                }
+        ).start();
+
+        new Thread(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        System.out.println(Thread.currentThread().getName() + ": Hello World");
+                    }
+                }
+        ).start();
+    }
+}
