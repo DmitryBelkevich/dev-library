552ece75799913f0cf1367cdf26efccc272ca499
hard <harleydav@mail.ru>
2018-03-07 08:12:45 +0300

    http-server

--- java9-core/multithreading/threads/src/main/java/com/hard/_10_examples/_02_htt_server2/Main.java
+++ java9-core/multithreading/threads/src/main/java/com/hard/_10_examples/_02_htt_server2/Main.java
@@ -16,63 +16,63 @@ public class Main {
             new Thread(new SocketProcessor(socket)).start();
         }
     }
+}
 
-    private static class SocketProcessor implements Runnable {
-        private Socket socket;
-        private InputStream inputStream;
-        private OutputStream outputStream;
+class SocketProcessor implements Runnable {
+    private Socket socket;
+    private InputStream inputStream;
+    private OutputStream outputStream;
 
-        private SocketProcessor(Socket socket) throws Throwable {
-            this.socket = socket;
-            this.inputStream = socket.getInputStream();
-            this.outputStream = socket.getOutputStream();
-        }
+    public SocketProcessor(Socket socket) throws Throwable {
+        this.socket = socket;
+        this.inputStream = socket.getInputStream();
+        this.outputStream = socket.getOutputStream();
+    }
 
-        public void run() {
+    public void run() {
+        try {
+            readInputHeaders();
+
+            String responseBody = "<html><body><h1>Hello World</h1></body></html>";
+            String responseHeaders =
+                    "HTTP/1.1 200 OK" + "\r\n"
+                            + "Server: YarServer/2009-09-09" + "\r\n"
+                            + "Content-Type: text/html" + "\r\n"
+                            + "Content-Length: " + responseBody.length() + "\r\n"
+                            + "Connection: close" + "\r\n"
+                            + "\r\n";
+
+            String response = responseHeaders + responseBody;
+
+            writeResponse(response);
+        } catch (Throwable t) {
+            /*do nothing*/
+        } finally {
             try {
-                readInputHeaders();
-
-                String responseBody = "<html><body><h1>Hello World</h1></body></html>";
-                String responseHeaders =
-                        "HTTP/1.1 200 OK" + "\r\n"
-                                + "Server: YarServer/2009-09-09" + "\r\n"
-                                + "Content-Type: text/html" + "\r\n"
-                                + "Content-Length: " + responseBody.length() + "\r\n"
-                                + "Connection: close" + "\r\n"
-                                + "\r\n";
-
-                String response = responseHeaders + responseBody;
-
-                writeResponse(response);
+                socket.close();
             } catch (Throwable t) {
                 /*do nothing*/
-            } finally {
-                try {
-                    socket.close();
-                } catch (Throwable t) {
-                    /*do nothing*/
-                }
             }
-
-            System.err.println("Client processing finished");
         }
 
-        private void writeResponse(String response) throws Throwable {
-            byte[] responseBytes = response.getBytes();
+        System.err.println("Client processing finished");
+    }
 
-            outputStream.write(responseBytes);
+    private void writeResponse(String response) throws Throwable {
+        byte[] responseBytes = response.getBytes();
 
-            outputStream.flush();
-        }
+        outputStream.write(responseBytes);
 
-        private void readInputHeaders() throws Throwable {
-            BufferedReader bufferedReaderSocket = new BufferedReader(new InputStreamReader(inputStream));
+        outputStream.flush();
+    }
 
-            while (true) {
-                String socketLine = bufferedReaderSocket.readLine();
-                if (socketLine == null || socketLine.trim().length() == 0)
-                    break;
-            }
+    private void readInputHeaders() throws Throwable {
+        BufferedReader bufferedReaderSocket = new BufferedReader(new InputStreamReader(inputStream));
+
+        while (true) {
+            String socketLine = bufferedReaderSocket.readLine();
+            if (socketLine == null || socketLine.trim().length() == 0)
+                break;
         }
     }
 }
