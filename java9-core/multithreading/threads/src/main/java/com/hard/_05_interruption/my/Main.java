86ec2ffb7744a07ce68be49d43bb1a1cf8e8bd92
hard <harleydav@mail.ru>
2018-03-08 08:50:26 +0300

    interruption

---
+++ java9-core/multithreading/threads/src/main/java/com/hard/_05_interruption/my/Main.java
@@ -0,0 +1,44 @@
+package com.hard._05_interruption.my;
+
+public class Main {
+    public static void main(String[] args) {
+        Entity entity = new Entity();
+
+        entity.start();
+
+        try {
+            Thread.sleep(2 * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        entity.setFinish(true);
+
+        System.out.println(Thread.currentThread().getName());
+    }
+}
+
+class Entity extends Thread {
+    private volatile boolean finish;
+
+    public void setFinish(boolean finish) {
+        this.finish = finish;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            if (!finish) {
+                System.out.println(Thread.currentThread().getName() + ": i = " + i);
+
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                return;
+            }
+        }
+    }
+}
