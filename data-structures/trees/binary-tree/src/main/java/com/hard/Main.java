169c86d59fe9ef3e63528bdfeb21a9b31750a32b
hard <harleydav@mail.ru>
2018-03-13 02:05:24 +0300

    init binary-tree module

---
+++ data-structures/trees/binary-tree/src/main/java/com/hard/Main.java
@@ -0,0 +1,17 @@
+package com.hard;
+
+public class Main {
+    public static void main(String[] args) {
+        Tree<Integer> tree = new Tree<>();
+    }
+}
+
+class Tree<T> {
+    private Node<T> root;
+
+    static class Node<T> {
+        private T data;
+        private Node<T> left;
+        private Node<T> right;
+    }
+}
