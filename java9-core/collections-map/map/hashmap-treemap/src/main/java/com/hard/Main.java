ab0fcd1ace58f613d42f67655d32409ff0c021f2
hard <harleydav@mail.ru>
2018-03-13 01:45:07 +0300

    hashmap-treemap

---
+++ java9-core/collections-map/map/hashmap-treemap/src/main/java/com/hard/Main.java
@@ -0,0 +1,31 @@
+package com.hard;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Main {
+    public static void main(String[] args) {
+        /**
+         * Основан на хеш-таблицах
+         */
+        Map<String, String> map = new HashMap<>();
+
+        /**
+         * Основан на красно-чёрном дереве
+         */
+//        Map<String, String> map = new TreeMap<>();
+
+        map.put("b", "bbb");
+        map.put("a", "aaa");
+        map.put("c", "ccc");
+
+        System.out.println(map);
+
+        for (String key : map.keySet()) {
+            String val = map.get(key);
+
+            System.out.println(key + ": " + val);
+        }
+    }
+}
