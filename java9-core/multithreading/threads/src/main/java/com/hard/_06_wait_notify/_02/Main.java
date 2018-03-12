3e73a9cee0a3cb13a731e123a0ca08dc99c36906
hard <harleydav@mail.ru>
2018-03-12 17:47:08 +0300

    wait-notify

---
+++ java9-core/multithreading/threads/src/main/java/com/hard/_06_wait_notify/_02/Main.java
@@ -0,0 +1,48 @@
+package com.hard._06_wait_notify._02;
+
+public class Main {
+    public static void main(String[] args) {
+        Object sync = new Object();
+
+        new Thread(() -> {
+            synchronized (sync) {
+                System.out.println(Thread.currentThread().getName() + ": is waiting");
+
+                try {
+                    sync.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                System.out.println(Thread.currentThread().getName() + ": end of waiting");
+            }
+        }).start();
+
+        new Thread(() -> {
+            synchronized (sync) {
+                System.out.println(Thread.currentThread().getName() + ": is waiting");
+
+                try {
+                    sync.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                System.out.println(Thread.currentThread().getName() + ": end of waiting");
+            }
+        }).start();
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        new Thread(() -> {
+            synchronized (sync) {
+                System.out.println(Thread.currentThread().getName() + ": notify");
+                sync.notifyAll();
+            }
+        }).start();
+    }
+}
