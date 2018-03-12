9554cef428433ec2fc2cdb6082de08732a2fca3c
hard <harleydav@mail.ru>
2018-03-12 17:48:32 +0300

    refactor

--- java9-core/multithreading/threads/src/main/java/com/hard/_10_examples/_02_htt_server2/Main.java
+++ java9-core/multithreading/threads/src/main/java/com/hard/_10_examples/_02_htt_server2/Main.java
@@ -1,6 +1,9 @@
 package com.hard._10_examples._02_htt_server2;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Collection;
