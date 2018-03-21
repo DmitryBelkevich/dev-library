64a963dbd471b514006335c06331aaadf52fdec4
hard <harleydav@mail.ru>
2018-03-21 17:17:40 +0300

    abstract-class-diff-interface module

--- oop/abstract-class-diff-interface/src/main/java/com/hard/Main.java
+++ oop/abstract-class-diff-interface/src/main/java/com/hard/Main.java
@@ -11,6 +11,7 @@ public class Main {
  * 2) может содержать только public static final поля: int a = 1;
  * 3) может описывать методы: void f();
  * 4) может реализовывать только static и default методы: static void f() {}; default void f() {}
+ * 5) может иметь модификаторы доступа только public
  *
  * Использование:
  * 1) могут быть реализованы классами которые не связаны друг с другом (class AndroidDevice implements Device; class IosDevice implements Device)
@@ -29,6 +30,7 @@ interface IEntity {
  * 2) может содержать поля
  * 3) может описывать методы: public abstract void f();
  * 4) может реализовывать методы: public void f() {}
+ * 5) может иметь любые модификаторы доступа
  *
  * Использование:
  * 1) используются только тогда, когда есть тип отношений "is a" (class AndroidMobile extends AndroidDevice; class AndroidTablet extends AndroidDevice)
