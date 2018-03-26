78daec4f385cf6b3283e8f56252f575cb02a0f35
hard <harleydav@mail.ru>
2018-03-27 01:27:06 +0300

    static

--- java9-core/keywords/static/src/main/java/com/hard/Main.java
+++ java9-core/keywords/static/src/main/java/com/hard/Main.java
@@ -6,8 +6,13 @@ public class Main {
     }
 }
 
-class Entity {
+abstract class Entity {
     public static String str;
+
+    /**
+     * статический блок служит для инициализации статических полей
+     * выполняется до выполнения конструктора
+     */
 
     static {
         System.out.println("static");
@@ -16,5 +21,33 @@ class Entity {
 
     public Entity() {
         System.out.println("Ctor");
+    }
+
+    /**
+     * статические методы не могут переопределяться в классах-наследниках
+     */
+
+    /**
+     * статические методы могут перегружаться нестатическими
+     */
+
+    public static void f1() {
+        System.out.println("f1");
+    }
+
+    public void f1(int a) {
+        System.out.println("f1");
+    }
+
+    /**
+     * нестатические методы могут перегружаться статическими
+     */
+
+    public void f2() {
+        System.out.println("f2");
+    }
+
+    public static void f2(int a) {
+        System.out.println("f2");
     }
 }
