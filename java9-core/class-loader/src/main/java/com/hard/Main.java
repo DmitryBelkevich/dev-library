d65d443aeb11383ecb1298f318665fe3659b04c4
hard <harleydav@mail.ru>
2018-03-27 03:34:27 +0300

    reflection

---
+++ java9-core/class-loader/src/main/java/com/hard/Main.java
@@ -0,0 +1,9 @@
+package com.hard;
+
+import java.net.URLClassLoader;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
--- java9-core/reflection/src/main/java/com/hard/Main.java
+++ java9-core/reflection/src/main/java/com/hard/Main.java
@@ -2,9 +2,22 @@ package com.hard;
 
 public class Main {
     public static void main(String[] args) {
+        // 1 - вызов из объекта
         Entity entity = new Entity();
-
         Class clazz = entity.getClass();
+
+        // 2 - вызов из статической переменной class, которая есть у каждого класса
+//        Class<Entity> clazz = Entity.class;
+
+        // 3
+//        Class<?> clazz = null;
+//        try {
+//            clazz = Class.forName("com.hard.Entity");
+//        } catch (ClassNotFoundException e) {
+//            e.printStackTrace();
+//        }
+
+        System.out.println();
     }
 }
 
