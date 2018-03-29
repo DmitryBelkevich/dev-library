c0d2aa367bd8ad9dc8f0f745986e6c7dd98dcf8e
hard <harleydav@mail.ru>
2018-03-29 16:27:36 +0300

    refactoring

--- java9-core/class-loader/src/main/java/com/hard/Main.java
+++ java9-core/class-loader/src/main/java/com/hard/Main.java
@@ -1,9 +1,42 @@
 package com.hard;
 
-import java.net.URLClassLoader;
-
 public class Main {
     public static void main(String[] args) {
 
     }
 }
+
+/**
+ * Java 8
+ *
+ * static class sun.misc.Launcher.AppClassLoader
+ *      extends class java.net.URLClassLoader
+ *           extends class java.security.SecureClassLoader
+ *               extends abstract class java.lang.ClassLoader
+ *
+ * static class sun.misc.Launcher.ExtClassLoader
+ *      extends class java.net.URLClassLoader
+ *           extends class java.security.SecureClassLoader
+ *               extends abstract class java.lang.ClassLoader
+ *
+ * -------------
+ *
+ * Java 9
+ *
+ * class jdk.internal.loader.ClassLoaders.BootClassLoader
+ *      extends class jdk.internal.loader.BuiltinClassLoader
+ *          extends class java.security.SecureClassLoader
+ *              extends abstract class java.lang.ClassLoader
+ *
+ * class jdk.internal.loader.ClassLoaders.PlatformClassLoader
+ *      extends class jdk.internal.loader.BuiltinClassLoader
+ *          extends class java.security.SecureClassLoader
+ *              extends abstract class java.lang.ClassLoader
+ *
+ * class jdk.internal.loader.ClassLoaders.AppClassLoader
+ *      extends class jdk.internal.loader.BuiltinClassLoader
+ *          extends class java.security.SecureClassLoader
+ *              extends abstract class java.lang.ClassLoader
+ */
+
+
