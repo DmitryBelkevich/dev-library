09bd7c311a67913e1f3447a48ecb08ecebb70775
hard <harleydav@mail.ru>
2018-03-30 23:37:34 +0300

    refactoring

--- java9-core/class-loaders/src/main/java/com/hard/_01_classloaders/Main.java
+++ java9-core/class-loaders/src/main/java/com/hard/_01_classloaders/Main.java
@@ -3,18 +3,23 @@ package com.hard._01_classloaders;
 public class Main {
     public static void main(String[] args) {
         ClassLoader classLoader = Main.class.getClassLoader();
-        System.out.println(classLoader);    // sun.misc.Launcher$AppClassLoader
+
+        System.out.println(classLoader);
+        // java-8: sun.misc.Launcher$AppClassLoader
+        // java-9: jdk.internal.loader.ClassLoaders$AppClassLoader
     }
 }
 
 /**
  * Java 8
  *
+ * Extension class loader:
  * static class sun.misc.Launcher.ExtClassLoader
  *      extends class java.net.URLClassLoader
  *           extends class java.security.SecureClassLoader
  *               extends abstract class java.lang.ClassLoader
  *
+ * System class loader:
  * static class sun.misc.Launcher.AppClassLoader
  *      extends class java.net.URLClassLoader
  *           extends class java.security.SecureClassLoader
@@ -24,16 +29,19 @@ public class Main {
  *
  * Java 9
  *
+ * Bootstrap class loader:
  * class jdk.internal.loader.ClassLoaders.BootClassLoader
  *      extends class jdk.internal.loader.BuiltinClassLoader
  *          extends class java.security.SecureClassLoader
  *              extends abstract class java.lang.ClassLoader
  *
+ * Extension class loader:
  * class jdk.internal.loader.ClassLoaders.PlatformClassLoader
  *      extends class jdk.internal.loader.BuiltinClassLoader
  *          extends class java.security.SecureClassLoader
  *              extends abstract class java.lang.ClassLoader
  *
+ * System class loader:
  * class jdk.internal.loader.ClassLoaders.AppClassLoader
  *      extends class jdk.internal.loader.BuiltinClassLoader
  *          extends class java.security.SecureClassLoader
