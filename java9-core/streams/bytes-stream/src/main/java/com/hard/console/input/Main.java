34292803cb15a5adbd997abd6e68aedc349c6f20
hard <harleydav@mail.ru>
2018-04-04 02:39:02 +0300

    console input/output

---
+++ java9-core/streams/bytes-stream/src/main/java/com/hard/console/input/Main.java
@@ -0,0 +1,24 @@
+package com.hard.console.input;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = null;
+
+        inputStream = System.in;
+
+        InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+        String str = null;
+        try {
+            str = bufferedReader.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.print(str);
+    }
+}
---
+++ java9-core/streams/bytes-stream/src/main/java/com/hard/console/output/Main.java
@@ -0,0 +1,7 @@
+package com.hard.console.output;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Hello World");
+    }
+}
