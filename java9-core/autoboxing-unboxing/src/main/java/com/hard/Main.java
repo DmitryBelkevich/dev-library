73dabf6028dc85c008dcf2785907509d5b2142f6
hard <harleydav@mail.ru>
2018-04-06 23:54:45 +0300

    autoboxing/unboxing

---
+++ java9-core/autoboxing-unboxing/src/main/java/com/hard/Main.java
@@ -0,0 +1,12 @@
+package com.hard;
+
+public class Main {
+    public static void main(String[] args) {
+        int i = new Integer(1);
+    }
+}
+
+/**
+ * autoboxing / unboxing
+ * автоматическое преобразование между скалярными типами и соответствующими классами-обёртками
+ */
