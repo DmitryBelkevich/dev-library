0f58a67e7d35e6940491189dc400ce191f8151df
hard <harleydav@mail.ru>
2018-04-08 01:45:41 +0300

    hashmap--hashtable

--- data-structures/hashmap--hashtable/src/main/java/com/hard/Main.java
+++ data-structures/hashmap--hashtable/src/main/java/com/hard/Main.java
@@ -3,6 +3,9 @@ package com.hard;
 public class Main {
     public static void main(String[] args) {
         HashTable<Integer> hashTable = new HashTable<>();
+
+        for (int i = 0; i < 10; i++)
+            hashTable.add(i);
     }
 }
 
@@ -16,4 +19,21 @@ class HashTable<T> {
     }
 
     private Node<T>[] table;
+
+    public HashTable() {
+        table = new Node[size];
+    }
+
+    private int hashcode(int data) {
+        return data % size;
+    }
+
+    public void add(int data) {
+        int hashcode = this.hashcode(data);
+
+        Node node = new Node();
+        node.hashcode = hashcode;
+        node.data = data;
+        node.next = null;
+    }
 }
