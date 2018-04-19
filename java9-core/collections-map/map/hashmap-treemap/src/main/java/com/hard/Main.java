71e0fd77acb034f61ebeb6c49fb6b3a4c54d8f0a
hard <harleydav@mail.ru>
2018-04-20 00:02:59 +0300

    refactoring

--- java9-core/collections-map/map/hashmap-treemap/src/main/java/com/hard/Main.java
+++ java9-core/collections-map/map/hashmap-treemap/src/main/java/com/hard/Main.java
@@ -6,7 +6,7 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) {
         /**
-         * Основан на хеш-таблицах
+         * Основан на хеш-таблицах / хеш-картах
          */
         Map<String, String> map = new HashMap<>();
 
