c0686b1aacd2d6e6ab03027c11e8e9136c638025
hard <harleydav@mail.ru>
2018-04-25 05:41:41 +0300

    refactoring

--- programming-paradigms/functional-programming/object-function/src/main/java/com/hard/Main.java
+++ programming-paradigms/functional-programming/object-function/src/main/java/com/hard/Main.java
@@ -4,8 +4,8 @@ public class Main {
     public static void main(String[] args) {
         Function<Integer, String> intToString = new Function<Integer, String>() {
             @Override
-            public String apply(Integer t) {
-                return t.toString();
+            public String apply(Integer from) {
+                return from.toString();
             }
         };
 
@@ -14,6 +14,6 @@ public class Main {
     }
 }
 
-interface Function<T, R> {
-    R apply(T t);
+interface Function<F, T> {
+    T apply(F from);
 }
