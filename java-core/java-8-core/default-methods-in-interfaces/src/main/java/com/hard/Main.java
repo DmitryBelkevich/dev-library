8ab221ff1d440190ded4d26266c86aac6c4b2084
hard <harleydav@mail.ru>
2018-04-26 00:17:12 +0300

    default-methods

--- java-core/java-8-core/default-methods-in-interfaces/src/main/java/com/hard/Main.java
+++ java-core/java-8-core/default-methods-in-interfaces/src/main/java/com/hard/Main.java
@@ -2,6 +2,21 @@ package com.hard;
 
 public class Main {
     public static void main(String[] args) {
+        Listener listener = new Listener() {
+            @Override
+            public void execute() {
+                print();
+            }
+        };
 
+        listener.execute();
     }
+}
+
+interface Listener {
+    default void print() {
+        System.out.println("Hello World");
+    }
+
+    void execute();
 }
