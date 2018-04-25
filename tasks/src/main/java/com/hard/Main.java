6e15249605f8ea9fc41951f03f7aa67af64e9802
hard <harleydav@mail.ru>
2018-04-25 06:50:10 +0300

    refactoring

--- tasks/src/main/java/com/hard/Main.java
+++ tasks/src/main/java/com/hard/Main.java
@@ -1,23 +1,7 @@
 package com.hard;
 
-import java.util.ArrayList;
-import java.util.Collection;
-
 public class Main {
     public static void main(String[] args) {
-        Collection<String> collection = new ArrayList<>();
-
-        collection.add("aa1");
-        collection.add("cc2");
-        collection.add("cc1");
-        collection.add("aa2");
-        collection.add("bb1");
-
-        collection
-                .stream()
-                .filter(s -> s.startsWith("a"))
-                .map(String::toUpperCase)
-                .sorted()
-                .forEach(System.out::println);
+        System.out.println("Hello World");
     }
 }
