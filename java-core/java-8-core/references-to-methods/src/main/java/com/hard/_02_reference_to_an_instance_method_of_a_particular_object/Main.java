eb978cfa00d8090101de7c33d4eeae28b1da70cc
hard <harleydav@mail.ru>
2018-04-26 05:51:11 +0300

    references-to-methods

--- java-core/java-8-core/references-to-methods/src/main/java/com/hard/_02_reference_to_an_instance_method_of_a_particular_object/Main.java
+++ java-core/java-8-core/references-to-methods/src/main/java/com/hard/_02_reference_to_an_instance_method_of_a_particular_object/Main.java
@@ -2,6 +2,21 @@ package com.hard._02_reference_to_an_instance_method_of_a_particular_object;
 
 public class Main {
     public static void main(String[] args) {
+        Printer printer = new Printer();
 
+        Listener listener = printer::print;
+//        Listener listener = (str) -> System.out.println(str);
+
+        listener.execute("Hello World");
+    }
+}
+
+interface Listener {
+    void execute(String str);
+}
+
+class Printer {
+    public void print(String str) {
+        System.out.println(str);
     }
 }
