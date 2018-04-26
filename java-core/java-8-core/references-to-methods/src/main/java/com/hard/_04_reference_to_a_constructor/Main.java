ed3414aaf0149424745b0eb39cc7a33cb0e18e01
hard <harleydav@mail.ru>
2018-04-26 06:04:41 +0300

    references-to-methods

--- java-core/java-8-core/references-to-methods/src/main/java/com/hard/_04_reference_to_a_constructor/Main.java
+++ java-core/java-8-core/references-to-methods/src/main/java/com/hard/_04_reference_to_a_constructor/Main.java
@@ -2,6 +2,18 @@ package com.hard._04_reference_to_a_constructor;
 
 public class Main {
     public static void main(String[] args) {
+        Factory factory = Entity::new;
+//        Factory factory = () -> new Entity();
 
+        Entity entity = factory.execute();
+        System.out.println(entity);
     }
+}
+
+interface Factory {
+    Entity execute();
+}
+
+class Entity {
+
 }
