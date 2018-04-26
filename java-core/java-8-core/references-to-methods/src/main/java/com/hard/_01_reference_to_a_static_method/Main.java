6d2694ef2d1f51075d147f6369b3ef2cdc0046eb
hard <harleydav@mail.ru>
2018-04-26 03:23:32 +0300

    references-to-static-methods

--- java-core/java-8-core/references-to-methods/src/main/java/com/hard/_01_reference_to_a_static_method/Main.java
+++ java-core/java-8-core/references-to-methods/src/main/java/com/hard/_01_reference_to_a_static_method/Main.java
@@ -2,6 +2,19 @@ package com.hard._01_reference_to_a_static_method;
 
 public class Main {
     public static void main(String[] args) {
+        Listener listener = Printer::print;
+//        Listener listener = (str) -> System.out.println(str);
 
+        listener.execute("Hello World");
+    }
+}
+
+interface Listener {
+    void execute(String str);
+}
+
+class Printer {
+    public static void print(String str) {
+        System.out.println(str);
     }
 }
