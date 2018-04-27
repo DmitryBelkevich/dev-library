b3fd656620ae71ba285281ddcd828641b21b2862
hard <harleydav@mail.ru>
2018-04-27 15:53:27 +0300

    references-to-methods

--- java-core/java-8-core/references-to-methods/src/main/java/com/hard/_03_reference_to_an_instance_method_of_an_arbitrary_object_of_a_particular_type/Main.java
+++ java-core/java-8-core/references-to-methods/src/main/java/com/hard/_03_reference_to_an_instance_method_of_an_arbitrary_object_of_a_particular_type/Main.java
@@ -1,7 +1,16 @@
 package com.hard._03_reference_to_an_instance_method_of_an_arbitrary_object_of_a_particular_type;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+        String[] stringArray = {
+                "c", "f", "b", "e", "a", "d"
+        };
 
+        Arrays.sort(stringArray, String::compareToIgnoreCase);
+
+        for (String str : stringArray)
+            System.out.println(str);
     }
 }
