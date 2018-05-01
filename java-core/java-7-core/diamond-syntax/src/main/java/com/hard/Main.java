4e1a4b7a60bc0b28d25916cb199879575b727407
hard <harleydav@mail.ru>
2018-05-01 16:11:11 +0300

    diamonds

---
+++ java-core/java-7-core/diamond-syntax/src/main/java/com/hard/Main.java
@@ -0,0 +1,10 @@
+package com.hard;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class Main {
+    public static void main(String[] args) {
+        Collection<String> collection = new ArrayList<>();
+    }
+}
