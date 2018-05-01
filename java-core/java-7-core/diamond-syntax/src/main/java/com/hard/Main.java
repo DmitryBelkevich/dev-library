ebf7c661d9fb71921f4c37fd8e831dc6755f8fab
hard <harleydav@mail.ru>
2018-05-01 16:16:19 +0300

    refactoring

--- java-core/java-7-core/diamond-syntax/src/main/java/com/hard/Main.java
+++ java-core/java-7-core/diamond-syntax/src/main/java/com/hard/Main.java
@@ -5,6 +5,9 @@ import java.util.Collection;
 
 public class Main {
     public static void main(String[] args) {
+        /**
+         * можно опустить параметры типа. Т.е. можно предоставить компилятору определение типов при создании объекта
+         */
         Collection<String> collection = new ArrayList<>();
     }
 }
