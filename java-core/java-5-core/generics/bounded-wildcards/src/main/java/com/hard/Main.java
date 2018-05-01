bc075d2ad3dd6ecb0bc3f0cff0ba22b0ce386639
hard <harleydav@mail.ru>
2018-05-01 18:32:59 +0300

    bounded-wildcards

---
+++ java-core/java-5-core/generics/bounded-wildcards/src/main/java/com/hard/Main.java
@@ -0,0 +1,27 @@
+package com.hard;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class Main {
+    public static void main(String[] args) {
+        Collection<Integer> collection = new ArrayList<>();
+        collection.add(1);
+        collection.add(2);
+        collection.add(3);
+
+        print(collection);
+
+        Collection<Double> collection2 = new ArrayList<>();
+        collection2.add(1.0);
+        collection2.add(2.0);
+        collection2.add(3.0);
+
+        print(collection2);
+    }
+
+    private static void print(Collection<? extends Number> numbers) {
+        for (Number number : numbers)
+            System.out.println(number);
+    }
+}
