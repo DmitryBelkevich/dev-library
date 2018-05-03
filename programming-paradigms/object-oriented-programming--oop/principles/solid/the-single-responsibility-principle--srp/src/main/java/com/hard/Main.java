d320a8d3b50a1dbdf52cd213da25dfa5c697eab8
hard <harleydav@mail.ru>
2018-05-03 22:57:06 +0300

    the-single-responsibility-principle

--- programming-paradigms/object-oriented-programming--oop/principles/solid/the-single-responsibility-principle--srp/src/main/java/com/hard/Main.java
+++ programming-paradigms/object-oriented-programming--oop/principles/solid/the-single-responsibility-principle--srp/src/main/java/com/hard/Main.java
@@ -5,3 +5,46 @@ public class Main {
 
     }
 }
+
+/**
+ * Before
+ */
+
+//class Computer {
+//    public void showImage() {
+//        System.out.println("show image");
+//    }
+//
+//    public void playMusic() {
+//        System.out.println("play music");
+//    }
+//}
+
+/**
+ * After
+ */
+
+class Display {
+    public void showImage() {
+        System.out.println("show image");
+    }
+}
+
+class Player {
+    public void playMusic() {
+        System.out.println("play music");
+    }
+}
+
+class Computer {
+    private Display display;
+    private Player player;
+
+    public void showImage() {
+        display.showImage();
+    }
+
+    public void playMusic() {
+        player.playMusic();
+    }
+}
