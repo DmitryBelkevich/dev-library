eb93fdb978afc9e8d1dcc77bcf9f952e4987a0e1
hard <harleydav@mail.ru>
2018-05-03 18:30:01 +0300

    static-and-dynamic-binding

--- programming-paradigms/object-oriented-programming--oop/polymorphism/static-and-dynamic-binding/src/main/java/com/hard/Main.java
+++ programming-paradigms/object-oriented-programming--oop/polymorphism/static-and-dynamic-binding/src/main/java/com/hard/Main.java
@@ -2,6 +2,34 @@ package com.hard;
 
 public class Main {
     public static void main(String[] args) {
+        B obj = new B();
 
+        System.out.println(execute(obj));
+    }
+
+    private static String execute(A obj) {
+        return obj.get(); // будет вызван метод get() из экземпляра класса B
     }
 }
+
+class A {
+    public String get() { // final - установит раннее связывание
+        return "A";
+    }
+}
+
+class B extends A {
+    @Override
+    public String get() {
+        return "B";
+    }
+}
+
+/**
+ * раннее связывание
+ * во время компиляции указывается, метод какого класса будет вызван в методе: String execute(A obj), т.е. метод экземпляра класса A
+ *
+ * позднее связывание
+ * во время компиляции не указывается, метод какого класса будет вызван в методе: String execute(A obj),
+ * и во время выполнения будет производиться поиск метода в объекте класса и после вызов её
+ */
