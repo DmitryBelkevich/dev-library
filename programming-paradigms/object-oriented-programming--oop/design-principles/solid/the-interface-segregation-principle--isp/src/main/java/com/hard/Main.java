e33c2bcf38fa6075079cf630faa376f1d39e4bee
hard <harleydav@mail.ru>
2018-05-04 18:44:57 +0300

    the-interface-segregation-principle

--- programming-paradigms/object-oriented-programming--oop/design-principles/solid/the-interface-segregation-principle--isp/src/main/java/com/hard/Main.java
+++ programming-paradigms/object-oriented-programming--oop/design-principles/solid/the-interface-segregation-principle--isp/src/main/java/com/hard/Main.java
@@ -2,13 +2,44 @@ package com.hard;
 
 public class Main {
     public static void main(String[] args) {
-
+        Worker worker = new Cook();
+        worker.work();
+        worker.sleep();
     }
 }
 
 /**
  * Bad example
  */
+
+interface Worker {
+    void work();
+    void sleep();
+}
+
+class Cook implements Worker {
+    @Override
+    public void work() {
+        System.out.println("Cook is working");
+    }
+
+    @Override
+    public void sleep() {
+        System.out.println("Cook is sleeping");
+    }
+}
+
+class Waiter implements Worker {
+    @Override
+    public void work() {
+        System.out.println("Waiter is working");
+    }
+
+    @Override
+    public void sleep() {
+
+    }
+}
 
 /**
  * Good example
