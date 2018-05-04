a5be834047a977598a93af58237d07ace2f60a2e
hard <harleydav@mail.ru>
2018-05-04 15:23:56 +0300

    the-liskov-substitution-principle

--- programming-paradigms/object-oriented-programming--oop/design-principles/solid/the-liskov-substitution-principle--lsp/src/main/java/com/hard/Main.java
+++ programming-paradigms/object-oriented-programming--oop/design-principles/solid/the-liskov-substitution-principle--lsp/src/main/java/com/hard/Main.java
@@ -2,6 +2,57 @@ package com.hard;
 
 public class Main {
     public static void main(String[] args) {
+        Rectangle square = new Square();
 
+        square.setWidth(2);
+
+        System.out.println(square.square());
     }
 }
+
+/**
+ * Bad example
+ */
+
+class Rectangle {
+    private int width;
+    private int height;
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int square() {
+        return width * height;
+    }
+}
+
+class Square extends Rectangle {
+    @Override
+    public void setWidth(int width) {
+        super.setWidth(width);
+        super.setHeight(width);
+    }
+
+    @Override
+    public void setHeight(int height) {
+        super.setHeight(height);
+        super.setWidth(height);
+    }
+}
+
+/**
+ * Good example
+ */
