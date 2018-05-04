3c8e01f4a2098f6b4d3d6200755a88de650ff8e1
hard <harleydav@mail.ru>
2018-05-04 15:28:50 +0300

    the-liskov-substitution-principle

--- programming-paradigms/object-oriented-programming--oop/design-principles/solid/the-liskov-substitution-principle--lsp/src/main/java/com/hard/Main.java
+++ programming-paradigms/object-oriented-programming--oop/design-principles/solid/the-liskov-substitution-principle--lsp/src/main/java/com/hard/Main.java
@@ -2,7 +2,7 @@ package com.hard;
 
 public class Main {
     public static void main(String[] args) {
-        Rectangle square = new Square();
+        Square square = new Square();
 
         square.setWidth(2);
 
@@ -14,7 +14,54 @@ public class Main {
  * Bad example
  */
 
-class Rectangle {
+//class Rectangle {
+//    private int width;
+//    private int height;
+//
+//    public int getWidth() {
+//        return width;
+//    }
+//
+//    public void setWidth(int width) {
+//        this.width = width;
+//    }
+//
+//    public int getHeight() {
+//        return height;
+//    }
+//
+//    public void setHeight(int height) {
+//        this.height = height;
+//    }
+//
+//    public int square() {
+//        return width * height;
+//    }
+//}
+//
+//class Square extends Rectangle {
+//    @Override
+//    public void setWidth(int width) {
+//        super.setWidth(width);
+//        super.setHeight(width);
+//    }
+//
+//    @Override
+//    public void setHeight(int height) {
+//        super.setHeight(height);
+//        super.setWidth(height);
+//    }
+//}
+
+/**
+ * Good example
+ */
+
+interface Shape {
+    int square();
+}
+
+class Rectangle implements Shape {
     private int width;
     private int height;
 
@@ -34,25 +81,25 @@ class Rectangle {
         this.height = height;
     }
 
+    @Override
     public int square() {
         return width * height;
     }
 }
 
-class Square extends Rectangle {
-    @Override
+class Square implements Shape {
+    private int width;
+
+    public int getWidth() {
+        return width;
+    }
+
     public void setWidth(int width) {
-        super.setWidth(width);
-        super.setHeight(width);
+        this.width = width;
     }
 
     @Override
-    public void setHeight(int height) {
-        super.setHeight(height);
-        super.setWidth(height);
+    public int square() {
+        return width * width;
     }
 }
-
-/**
- * Good example
- */
