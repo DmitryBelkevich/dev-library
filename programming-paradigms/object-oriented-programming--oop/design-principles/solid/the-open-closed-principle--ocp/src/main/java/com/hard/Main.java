c8bffb8c08fc936af074c9a74b88fc7f63d8d4a0
hard <harleydav@mail.ru>
2018-05-04 13:21:31 +0300

    the-open-closed-principle

--- programming-paradigms/object-oriented-programming--oop/design-principles/solid/the-open-closed-principle--ocp/src/main/java/com/hard/Main.java
+++ programming-paradigms/object-oriented-programming--oop/design-principles/solid/the-open-closed-principle--ocp/src/main/java/com/hard/Main.java
@@ -12,35 +12,59 @@ public class Main {
  * Bad example
  */
 
-interface Shape {
-
-}
-
-class Circle implements Shape {
-
-}
-
-class Rectangle implements Shape {
-
-}
-
-class GraphicEditor {
-    public void drawShape(Shape shape) {
-        if (shape instanceof Circle)
-            drawCircle((Circle) shape);
-        else if (shape instanceof Rectangle)
-            drawRectangle((Rectangle) shape);
-    }
-
-    public void drawCircle(Circle circle) {
-        System.out.println("drawing circle");
-    }
-
-    public void drawRectangle(Rectangle rectangle) {
-        System.out.println("drawing rectangle");
-    }
-}
+//interface Shape {
+//
+//}
+//
+//class Circle implements Shape {
+//
+//}
+//
+//class Rectangle implements Shape {
+//
+//}
+//
+//class GraphicEditor {
+//    public void drawShape(Shape shape) {
+//        if (shape instanceof Circle)
+//            drawCircle((Circle) shape);
+//        else if (shape instanceof Rectangle)
+//            drawRectangle((Rectangle) shape);
+//    }
+//
+//    public void drawCircle(Circle circle) {
+//        System.out.println("drawing circle");
+//    }
+//
+//    public void drawRectangle(Rectangle rectangle) {
+//        System.out.println("drawing rectangle");
+//    }
+//}
 
 /**
  * Good example
  */
+
+interface Shape {
+    void draw();
+}
+
+class Circle implements Shape {
+    @Override
+    public void draw() {
+        System.out.println("drawing circle");
+    }
+}
+
+class Rectangle implements Shape {
+    @Override
+    public void draw() {
+        System.out.println("drawing rectangle");
+    }
+}
+
+class GraphicEditor {
+    public void drawShape(Shape shape) {
+        shape.draw();
+    }
+}
