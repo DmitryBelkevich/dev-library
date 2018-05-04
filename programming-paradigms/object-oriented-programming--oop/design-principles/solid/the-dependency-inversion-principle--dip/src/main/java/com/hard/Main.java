78458c919da19b41c5ad806ba21ce0b4c9467fa2
hard <harleydav@mail.ru>
2018-05-04 23:54:34 +0300

    the-dependency-inversion-principle

--- programming-paradigms/object-oriented-programming--oop/design-principles/solid/the-dependency-inversion-principle--dip/src/main/java/com/hard/Main.java
+++ programming-paradigms/object-oriented-programming--oop/design-principles/solid/the-dependency-inversion-principle--dip/src/main/java/com/hard/Main.java
@@ -2,7 +2,9 @@ package com.hard;
 
 public class Main {
     public static void main(String[] args) {
+        ProductService productService = new ProductService();
 
+        productService.getAll();
     }
 }
 
@@ -10,6 +12,39 @@ public class Main {
  * Bad example
  */
 
+//class ProductDao {
+//    public void getAll() {
+//        System.out.println("getAll");
+//    }
+//}
+//
+//class ProductService {
+//    private ProductDao productDao = new ProductDao();
+//
+//    public void getAll() {
+//        productDao.getAll();
+//    }
+//}
+
 /**
  * Good example
  */
+
+interface Dao {
+    void getAll();
+}
+
+class ProductDao implements Dao {
+    @Override
+    public void getAll() {
+        System.out.println("getAll");
+    }
+}
+
+class ProductService {
+    private Dao productDao = new ProductDao();
+
+    public void getAll() {
+        productDao.getAll();
+    }
+}
