c78cf2143ed9475af3be66e278fc35ca51786c7b
hard <harleydav@mail.ru>
2018-05-04 13:03:27 +0300

    the-open-closed-principle

--- programming-paradigms/object-oriented-programming--oop/design-principles/solid/the-open-closed-principle--ocp/src/main/java/com/hard/Main.java
+++ programming-paradigms/object-oriented-programming--oop/design-principles/solid/the-open-closed-principle--ocp/src/main/java/com/hard/Main.java
@@ -2,6 +2,45 @@ package com.hard;
 
 public class Main {
     public static void main(String[] args) {
+        GraphicEditor graphicEditor = new GraphicEditor();
 
+        graphicEditor.drawShape(new Circle());
+    }
+}
+
+/**
+ * Bad example
+ */
+
+class GraphicEditor {
+    public void drawShape(Shape shape) {
+        if (shape.type == 1)
+            drawCircle((Circle) shape);
+        else if (shape.type == 2)
+            drawRectangle((Rectangle) shape);
+    }
+
+    public void drawCircle(Circle circle) {
+        System.out.println("drawing circle");
+    }
+
+    public void drawRectangle(Rectangle rectangle) {
+        System.out.println("drawing rectangle");
+    }
+}
+
+class Shape {
+    protected int type;
+}
+
+class Circle extends Shape {
+    public Circle() {
+        type = 1;
+    }
+}
+
+class Rectangle extends Shape {
+    public Rectangle() {
+        type = 2;
     }
 }
