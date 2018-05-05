23b1956a4722981b4d1fd3dbf33ed07d26da8c56
hard <harleydav@mail.ru>
2018-05-05 22:05:36 +0300

    factorial

--- tasks/factorial/src/test/java/com/hard/ArithmeticTest.java
+++ tasks/factorial/src/test/java/com/hard/ArithmeticTest.java
@@ -5,27 +5,27 @@ import org.junit.Test;
 
 public class ArithmeticTest {
     @Test
-    public void test1() {
+    public void should_return_result_from_factorial_1() {
         Assert.assertEquals(1, Arithmetic.factorial(1));
     }
 
     @Test
-    public void test2() {
+    public void should_return_result_from_factorial_2() {
         Assert.assertEquals(1 * 2, Arithmetic.factorial(2));
     }
 
     @Test
-    public void test3() {
+    public void should_return_result_from_factorial_3() {
         Assert.assertEquals(1 * 2 * 3, Arithmetic.factorial(3));
     }
 
     @Test
-    public void test4() {
+    public void should_return_result_from_factorial_4() {
         Assert.assertEquals(1 * 2 * 3 * 4, Arithmetic.factorial(4));
     }
 
     @Test
-    public void test5() {
+    public void should_return_result_from_factorial_5() {
         Assert.assertEquals(1 * 2 * 3 * 4 * 5, Arithmetic.factorial(5));
     }
 }
