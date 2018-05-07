9e8f9d1b6f98f32fc6e82f8dc4f52251e79e06c7
hard <harleydav@mail.ru>
2018-05-07 19:08:20 +0300

    refactoring

---
+++ programming-paradigms/object-oriented-programming--oop/design-principles/solid/the-open-closed-principle--ocp/src/main/java/com/hard/example2/Main.java
@@ -0,0 +1,45 @@
+package com.hard.example2;
+
+public class Main {
+    public static void main(String[] args) {
+        OrderService orderService = new OrderService();
+
+        orderService.calculateTotalPrice();
+    }
+}
+
+/**
+ * Bad example
+ */
+
+//class OrderService {
+//    public void getTotalPrice() {
+//        System.out.println("get all positions of cart");
+//        System.out.println("get discount for current user");
+//        System.out.println("count of total price");
+//    }
+//}
+
+/**
+ * Good example
+ */
+
+class OrderService {
+    public void getTotalPrice() {
+        getProducts();
+        getDiscount();
+        calculateTotalPrice();
+    }
+
+    public void getProducts() {
+        System.out.println("get all positions of cart");
+    }
+
+    public void getDiscount() {
+        System.out.println("get discount for current user");
+    }
+
+    public void calculateTotalPrice() {
+        System.out.println("count of total price");
+    }
+}
