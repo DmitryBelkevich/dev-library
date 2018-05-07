f777c1b1799b2bfc63bd5f0f718c3d089ddefabc
hard <harleydav@mail.ru>
2018-05-08 02:46:14 +0300

    Inversion of Control (IoC)

---
+++ programming-paradigms/object-oriented-programming--oop/design-principles/inversion-of-control--ioc/src/main/java/com/hard/_03_dependency_injection/_03_interface_injection/Main.java
@@ -0,0 +1,51 @@
+package com.hard._03_dependency_injection._03_interface_injection;
+
+public class Main {
+    public static void main(String[] args) {
+        ProductController productController = new ProductController();
+
+        productController.inject(new ProductServiceImpl());
+
+        productController.getAll();
+    }
+}
+
+/**
+ * Service
+ */
+
+interface ProductService {
+    void getAll();
+}
+
+/**
+ * Concrete Service
+ */
+
+class ProductServiceImpl implements ProductService {
+    @Override
+    public void getAll() {
+        System.out.println("getAll");
+    }
+}
+
+/**
+ * Inversion of Control (IoC)
+ */
+
+interface Injector {
+    void inject(ProductService productService);
+}
+
+class ProductController implements Injector {
+    private ProductService productService;
+
+    @Override
+    public void inject(ProductService productService) {
+        this.productService = productService;
+    }
+
+    public void getAll() {
+        productService.getAll();
+    }
+}
