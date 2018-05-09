d93bcda939e9d9a8acbb0f0d1682551b2a71abea
hard <harleydav@mail.ru>
2018-05-10 00:21:15 +0300

    Launcher

--- tasks/game-development/src/main/java/com/hard/_02_launcher/Main.java
+++ tasks/game-development/src/main/java/com/hard/_02_launcher/Main.java
@@ -4,6 +4,82 @@ public class Main {
     public static void main(String[] args) {
         DesktopLauncher desktopLauncher = new DesktopLauncher();
         desktopLauncher.main();
+
+        AndroidLauncher androidLauncher = new AndroidLauncher();
+        androidLauncher.onCreate();
+
+        HtmlLauncher htmlLauncher = new HtmlLauncher();
+        htmlLauncher.create();
+    }
+}
+
+/**
+ * Game core
+ */
+
+class Game {
+    public static final int WIDTH = 480;
+    public static final int HEIGHT = 800;
+    public static final String title = "Game";
+}
+
+/**
+ * Config
+ */
+
+class DesktopConfig {
+    public int width;
+    public int height;
+    public String title;
+}
+
+class AndroidConfig {
+
+}
+
+class HtmlConfig {
+    public int width;
+    public int height;
+    public String title;
+}
+
+/**
+ * Application
+ */
+
+class DesktopApplication {
+    private Game game;
+    private DesktopConfig config;
+
+    public DesktopApplication(Game game, DesktopConfig config) {
+        this.game = game;
+        this.config = config;
+
+        System.out.println("App has launched on Desktop");
+    }
+}
+
+class AndroidApplication {
+    private Game game;
+    private AndroidConfig config;
+
+    public AndroidApplication(Game game, AndroidConfig config) {
+        this.game = game;
+        this.config = config;
+
+        System.out.println("App has launched on Android");
+    }
+}
+
+class HtmlApplication {
+    private Game game;
+    private HtmlConfig config;
+
+    public HtmlApplication(Game game, HtmlConfig config) {
+        this.game = game;
+        this.config = config;
+
+        System.out.println("App has launched on Html");
     }
 }
 
@@ -13,18 +89,36 @@ public class Main {
 
 class DesktopLauncher {
     public void main() {
-        System.out.println("App has launched on Desktop");
+        DesktopConfig config = new DesktopConfig();
+        config.width = Game.WIDTH;
+        config.height = Game.HEIGHT;
+        config.title = Game.title;
+
+        Game game = new Game();
+
+        new DesktopApplication(game, config);
     }
 }
 
 class AndroidLauncher {
     public void onCreate() {
-        System.out.println("App has launched on Android");
+        AndroidConfig config = new AndroidConfig();
+
+        Game game = new Game();
+
+        new AndroidApplication(game, config);
     }
 }
 
 class HtmlLauncher {
     public void create() {
-        System.out.println("App has launched on Html");
+        HtmlConfig config = new HtmlConfig();
+        config.width = Game.WIDTH;
+        config.height = Game.HEIGHT;
+        config.title = Game.title;
+
+        Game game = new Game();
+
+        new HtmlApplication(game, config);
     }
 }
