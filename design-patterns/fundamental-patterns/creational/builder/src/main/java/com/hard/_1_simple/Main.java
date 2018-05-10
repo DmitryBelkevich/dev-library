38b3376208d31e410421a53403342bc6e042bc79
hard <harleydav@mail.ru>
2018-05-11 00:47:07 +0300

    refactoring

--- design-patterns/fundamental-patterns/creational/builder/src/main/java/com/hard/_1_simple/Main.java
+++ design-patterns/fundamental-patterns/creational/builder/src/main/java/com/hard/_1_simple/Main.java
@@ -23,16 +23,16 @@ class Entity {
         return id;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public void setId(int id) {
-        this.id = id;
     }
 
     @Override
