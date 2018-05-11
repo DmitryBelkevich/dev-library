e2ebb09155750e38ed5ad2a48e51b23e18c404dc
hard <harleydav@mail.ru>
2018-05-11 04:43:10 +0300

    refactoring

--- tasks/game-development/src/main/java/com/hard/_00_architecture/Main.java
+++ tasks/game-development/src/main/java/com/hard/_00_architecture/Main.java
@@ -177,3 +177,127 @@ class GameFactory {
         return null;
     }
 }
+
+/**
+ * Config (Adapter)
+ */
+
+class DesktopConfig {
+    public int width;
+    public int height;
+    public String title;
+}
+
+class AndroidConfig {
+
+}
+
+class HtmlConfig {
+    public int width;
+    public int height;
+    public String title;
+}
+
+/**
+ * Application (Target)
+ */
+
+class DesktopApplication {
+    private Game game;
+    private DesktopConfig config;
+
+    public DesktopApplication(Game game, DesktopConfig config) {
+        this.game = game;
+        this.config = config;
+
+        init();
+    }
+
+    public void init() {
+        System.out.println("App has launched on Desktop");
+        System.out.println("run swing frame");
+
+        game.run();
+    }
+}
+
+class AndroidApplication {
+    private Game game;
+    private AndroidConfig config;
+
+    public AndroidApplication(Game game, AndroidConfig config) {
+        this.game = game;
+        this.config = config;
+
+        init();
+    }
+
+    public void init() {
+        System.out.println("App has launched on Android");
+        System.out.println("run main activity");
+
+        game.run();
+    }
+}
+
+class HtmlApplication {
+    private Game game;
+    private HtmlConfig config;
+
+    public HtmlApplication(Game game, HtmlConfig config) {
+        this.game = game;
+        this.config = config;
+
+        init();
+    }
+
+    public void init() {
+        System.out.println("App has launched on Html");
+        System.out.println("run window");
+
+        game.run();
+    }
+}
+
+/**
+ * Launcher (Client to target)
+ */
+
+class DesktopLauncher {
+    public void main() {
+        DesktopConfig config = new DesktopConfig();
+        config.width = Game.WIDTH;
+        config.height = Game.HEIGHT;
+        config.title = Game.title;
+
+        GameFactory gameFactory = new GameFactory();
+        Game game = gameFactory.getGame("Game1");
+
+        new DesktopApplication(game, config);
+    }
+}
+
+class AndroidLauncher {
+    public void onCreate() {
+        AndroidConfig config = new AndroidConfig();
+
+        GameFactory gameFactory = new GameFactory();
+        Game game = gameFactory.getGame("Game1");
+
+        new AndroidApplication(game, config);
+    }
+}
+
+class HtmlLauncher {
+    public void create() {
+        HtmlConfig config = new HtmlConfig();
+        config.width = Game.WIDTH;
+        config.height = Game.HEIGHT;
+        config.title = Game.title;
+
+        GameFactory gameFactory = new GameFactory();
+        Game game = gameFactory.getGame("Game1");
+
+        new HtmlApplication(game, config);
+    }
+}
