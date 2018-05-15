b2286e1593a9a10553ca00fb25af8091aa7b6a6a
hard <harleydav@mail.ru>
2018-05-15 03:21:05 +0300

    refactoring

--- java-core/java-2-core/swing/src/main/java/com/hard/_03_panel/_02_drawing/_01/Main.java
+++ java-core/java-2-core/swing/src/main/java/com/hard/_03_panel/_02_drawing/_01/Main.java
@@ -1,4 +1,4 @@
-package com.hard._03_panel._02_drawing._01_paint._02;
+package com.hard._03_panel._02_drawing._01;
 
 import javax.swing.*;
 import java.awt.*;
@@ -6,27 +6,24 @@ import java.awt.image.BufferedImage;
 
 public class Main {
     public static void main(String[] args) {
-        JFrame frame = new JFrame();
+        // creation GUI
+
+        JFrame frame = new JFrame("App");
 
         frame.setSize(640, 480);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
 
-        frame.setContentPane(new Panel());
-
-//        frame.pack();
         frame.setVisible(true);
-    }
-}
 
-class Panel extends JPanel {
-    @Override
-    public void paint(Graphics graphics) {
+        // drawing
+
         BufferedImage bufferedImage = new BufferedImage(200, 100, BufferedImage.TYPE_INT_RGB);
         Graphics2D graphics2D = (Graphics2D) bufferedImage.getGraphics();
 
         graphics2D.drawString("Hello World", 0, 10);
 
+        Graphics graphics = frame.getGraphics();
         graphics.drawImage(bufferedImage, 50, 50, null);
         graphics.dispose();
     }
---
+++ java-core/java-2-core/swing/src/main/java/com/hard/_03_panel/_02_drawing/_02_paint/_01/Main.java
@@ -0,0 +1,26 @@
+package com.hard._03_panel._02_drawing._02_paint._01;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+
+        frame.setSize(640,480);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+
+        frame.setContentPane(new Panel());
+
+//        frame.pack();
+        frame.setVisible(true);
+    }
+}
+
+class Panel extends JPanel {
+    @Override
+    public void paint(Graphics graphics) {
+        graphics.drawString("Hello World", 0, 10);
+    }
+}
