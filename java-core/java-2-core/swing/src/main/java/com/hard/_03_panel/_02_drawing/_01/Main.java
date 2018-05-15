6bdf1357672d2588ed2299809c7fe5b36d84dbae
hard <harleydav@mail.ru>
2018-05-15 03:43:39 +0300

    refactoring

--- java-core/java-2-core/swing/src/main/java/com/hard/_03_panel/_02_drawing/_01/Main.java
+++ java-core/java-2-core/swing/src/main/java/com/hard/_03_panel/_02_drawing/_01/Main.java
@@ -1,14 +1,13 @@
-package com.hard._03_panel._02_drawing._02_paint._02;
+package com.hard._03_panel._02_drawing._01;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.image.BufferedImage;
 
 public class Main {
     public static void main(String[] args) {
         JFrame frame = new JFrame();
 
-        frame.setSize(640, 480);
+        frame.setSize(640,480);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
 
@@ -22,12 +21,6 @@ public class Main {
 class Panel extends JPanel {
     @Override
     public void paint(Graphics graphics) {
-        BufferedImage bufferedImage = new BufferedImage(200, 100, BufferedImage.TYPE_INT_RGB);
-        Graphics2D graphics2D = (Graphics2D) bufferedImage.getGraphics();
-
-        graphics2D.drawString("Hello World", 0, 10);
-
-        graphics.drawImage(bufferedImage, 50, 50, null);
-        graphics.dispose();
+        graphics.drawString("Hello World", 0, 10);
     }
 }
--- java-core/java-2-core/swing/src/main/java/com/hard/_03_panel/_02_drawing/_02/Main.java
+++ java-core/java-2-core/swing/src/main/java/com/hard/_03_panel/_02_drawing/_02/Main.java
@@ -1,13 +1,14 @@
-package com.hard._03_panel._02_drawing._02_paint._01;
+package com.hard._03_panel._02_drawing._02;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 public class Main {
     public static void main(String[] args) {
         JFrame frame = new JFrame();
 
-        frame.setSize(640,480);
+        frame.setSize(640, 480);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
 
@@ -21,6 +22,12 @@ public class Main {
 class Panel extends JPanel {
     @Override
     public void paint(Graphics graphics) {
-        graphics.drawString("Hello World", 0, 10);
+        BufferedImage bufferedImage = new BufferedImage(200, 100, BufferedImage.TYPE_INT_RGB);
+        Graphics2D graphics2D = (Graphics2D) bufferedImage.getGraphics();
+
+        graphics2D.drawString("Hello World", 0, 10);
+
+        graphics.drawImage(bufferedImage, 50, 50, null);
+        graphics.dispose();
     }
 }
