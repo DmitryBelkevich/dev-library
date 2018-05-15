d185c8f0dda8bff5e41fac79793ecd49499b0632
hard <harleydav@mail.ru>
2018-05-15 19:42:20 +0300

    refactoring

--- design-patterns/fundamental-patterns/behavioral/memento/src/main/java/com/hard/example/Main.java
+++ design-patterns/fundamental-patterns/behavioral/memento/src/main/java/com/hard/example/Main.java
@@ -1,5 +1,7 @@
 package com.hard.example;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -35,11 +37,8 @@ public class Main {
  * State
  */
 
-abstract class State implements Cloneable {
-    @Override
-    protected Object clone() throws CloneNotSupportedException {
-        return super.clone();
-    }
+abstract class State {
+
 }
 
 class State1 extends State {
@@ -98,17 +97,47 @@ class StateManager {
     }
 
     public File createFile() {
-        State clonedState = null;
-        try {
-            clonedState = (State) state.clone();
-        } catch (CloneNotSupportedException e) {
-            e.printStackTrace();
-        }
+        State clonedState = (State) clone(state);
 
         return new File(clonedState);
     }
 
     public void loadFile(File file) {
         state = file.getState();
+    }
+
+    private Object clone(Object obj) {
+        try {
+            Class<?> clazz = obj.getClass();
+            Object clone = clazz.newInstance();
+
+            Field[] fields = clazz.getDeclaredFields();
+
+            for (Field field : fields) {
+                field.setAccessible(true);
+                if (field.get(obj) == null || Modifier.isFinal(field.getModifiers())) {
+                    continue;
+                }
+
+                if (field.getType().isPrimitive()
+                        || field.getType().equals(String.class)
+                        || field.getType().getSuperclass().equals(Number.class)
+                        || field.getType().equals(Boolean.class)) {
+                    field.set(clone, field.get(obj));
+                } else {
+                    Object childObj = field.get(obj);
+
+                    if (childObj == obj) {
+                        field.set(clone, clone);
+                    } else {
+                        field.set(clone, clone(field.get(obj)));
+                    }
+                }
+            }
+
+            return clone;
+        } catch (Exception e) {
+            return null;
+        }
     }
 }
