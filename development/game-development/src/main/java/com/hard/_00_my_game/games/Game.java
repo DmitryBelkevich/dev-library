1c4617210e7ee12ca566bd77046f9dae42e97181
hard <harleydav@mail.ru>
2018-05-15 06:41:37 +0300

    refactoring

--- development/game-development/src/main/java/com/hard/_00_my_game/games/Game.java
+++ development/game-development/src/main/java/com/hard/_00_my_game/games/Game.java
@@ -1,6 +1,33 @@
 package com.hard._00_my_game.games;
 
+import com.hard._00_my_game.GameFrame;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
 public abstract class Game {
+    // window
+    protected JFrame frame;
+
+    // settings
+
+    // graphics
+    protected BufferedImage bufferedImage;
+    protected Graphics graphics;
+
+    public Game() {
+        // window
+        GameFrame gameFrame = new GameFrame();
+        gameFrame.create();
+
+        this.frame = gameFrame.getFrame();
+
+        // graphics
+        this.bufferedImage = new BufferedImage(GameFrame.WIDTH, GameFrame.HEIGHT, BufferedImage.TYPE_INT_RGB);
+        this.graphics = bufferedImage.getGraphics();
+    }
+
     public abstract void run();
 
     public abstract void update();
--- development/game-development/src/main/java/com/hard/_00_my_game/games/game1/Game1.java
+++ development/game-development/src/main/java/com/hard/_00_my_game/games/game1/Game1.java
@@ -1,41 +1,17 @@
 package com.hard._00_my_game.games.game1;
 
-import com.hard._00_my_game.GameFrame;
+import com.hard._00_my_game.games.Game;
 import com.hard._00_my_game.games.game1.entities.Background;
 import com.hard._00_my_game.games.game1.entities.Entity;
 import com.hard._00_my_game.games.game1.entities.Player;
-import com.hard._00_my_game.games.Game;
 
-import javax.swing.*;
 import java.awt.*;
-import java.awt.image.BufferedImage;
 
 public class Game1 extends Game {
-    // window
-    private JFrame frame;
-
-    // settings
-
-    // graphics
-    private BufferedImage bufferedImage;
-    private Graphics graphics;
-
-    // entities
     private Entity background;
     private Entity player;
 
     public Game1() {
-        // window
-        GameFrame gameFrame = new GameFrame();
-        gameFrame.create();
-
-        this.frame = gameFrame.getFrame();
-
-        // graphics
-        this.bufferedImage = new BufferedImage(GameFrame.WIDTH, GameFrame.HEIGHT, BufferedImage.TYPE_INT_RGB);
-        this.graphics = bufferedImage.getGraphics();
-
-        // entities
         this.player = new Player(0, 0);
         this.background = new Background(0, 0);
     }
