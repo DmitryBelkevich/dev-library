7cdf7084b3ed5d00a3b1417633f5a3582286ddee
hard <harleydav@mail.ru>
2018-05-16 05:41:24 +0300

    refactoring

--- development/game-development/games/dragon-tale/src/main/java/com/hard/audio/AudioPlayer.java
+++ development/game-development/games/dragon-tale/src/main/java/com/hard/audio/AudioPlayer.java
@@ -1,37 +1,45 @@
 package com.hard.audio;
 
-import javax.sound.sampled.AudioFormat;
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
+import javax.sound.sampled.*;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class AudioPlayer {
     private Clip clip;
 
-    public AudioPlayer(String s) {
+    public AudioPlayer(String path) {
+        Class<?> clazz = getClass();
+        InputStream inputStream = clazz.getResourceAsStream(path);
+
+        AudioInputStream audioInputStream = null;
         try {
-            AudioInputStream ais =
-                    AudioSystem.getAudioInputStream(
-                            getClass().getResourceAsStream(
-                                    s
-                            )
-                    );
-            AudioFormat baseFormat = ais.getFormat();
-            AudioFormat decodeFormat = new AudioFormat(
-                    AudioFormat.Encoding.PCM_SIGNED,
-                    baseFormat.getSampleRate(),
-                    16,
-                    baseFormat.getChannels(),
-                    baseFormat.getChannels() * 2,
-                    baseFormat.getSampleRate(),
-                    false
-            );
-            AudioInputStream dais =
-                    AudioSystem.getAudioInputStream(
-                            decodeFormat, ais);
+            audioInputStream = AudioSystem.getAudioInputStream(inputStream);
+        } catch (UnsupportedAudioFileException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        AudioFormat baseFormat = audioInputStream.getFormat();
+        AudioFormat decodeFormat = new AudioFormat(
+                AudioFormat.Encoding.PCM_SIGNED,
+                baseFormat.getSampleRate(),
+                16,
+                baseFormat.getChannels(),
+                baseFormat.getChannels() * 2,
+                baseFormat.getSampleRate(),
+                false
+        );
+        AudioInputStream audioInputStream2 = AudioSystem.getAudioInputStream(decodeFormat, audioInputStream);
+        try {
             clip = AudioSystem.getClip();
-            clip.open(dais);
-        } catch (Exception e) {
+        } catch (LineUnavailableException e) {
+            e.printStackTrace();
+        }
+        try {
+            clip.open(audioInputStream2);
+        } catch (LineUnavailableException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
