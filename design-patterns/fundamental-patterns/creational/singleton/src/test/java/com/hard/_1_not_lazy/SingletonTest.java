69ad72fe09d993d92cf9cf04e3c68253e3eee049
hard <harleydav@mail.ru>
2018-05-17 02:41:15 +0300

    refactoring

--- design-patterns/fundamental-patterns/creational/singleton/src/test/java/com/hard/_1_not_lazy/SingletonTest.java
+++ design-patterns/fundamental-patterns/creational/singleton/src/test/java/com/hard/_1_not_lazy/SingletonTest.java
@@ -4,35 +4,15 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class SingletonTest {
+    /**
+     * проверка на Singleton
+     */
+
     @Test
-    public void shouldBeTheSameObjectByCreating() {
+    public void should_be_the_same_object_by_creating() {
         Singleton singleton1 = Singleton.getInstance();
         Singleton singleton2 = Singleton.getInstance();
 
-        Assert.assertEquals(singleton1, singleton2);
-    }
-
-    @Test
-    public void shouldWorkInMultithreading() {
-        Singleton singleton = Singleton.getInstance();
-
-        final int n = 1 * 1000;
-        for (int i = 0; i < n; i++) {
-            new Thread(() -> {
-                Assert.assertEquals(singleton, Singleton.getInstance());
-            }).start();
-        }
-    }
-}
-
-class Singleton {
-    private static Singleton instance = new Singleton();
-
-    private Singleton() {
-
-    }
-
-    public static Singleton getInstance() {
-        return instance;
+        Assert.assertSame(singleton1, singleton2);
     }
 }
