e08b7a3fe613bad8b45d30adc2a69162a0d3416d
hard <harleydav@mail.ru>
2018-05-17 02:44:03 +0300

    refactoring

---
+++ design-patterns/fundamental-patterns/creational/singleton/src/test/java/com/hard/_3_Initialization_on_Demand_Holder/SingletonTest.java
@@ -0,0 +1,18 @@
+package com.hard._3_Initialization_on_Demand_Holder;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SingletonTest {
+    /**
+     * проверка на Singleton
+     */
+
+    @Test
+    public void should_be_the_same_object_by_creating() {
+        Singleton singleton1 = Singleton.getInstance();
+        Singleton singleton2 = Singleton.getInstance();
+
+        Assert.assertSame(singleton1, singleton2);
+    }
+}
