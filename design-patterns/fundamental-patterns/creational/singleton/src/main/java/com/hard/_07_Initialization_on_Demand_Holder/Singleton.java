2ae2754ee9557b33047a616bd10b5d075d680a9e
hard <harleydav@mail.ru>
2018-05-17 04:34:06 +0300

    refactoring

--- design-patterns/fundamental-patterns/creational/singleton/src/main/java/com/hard/_07_Initialization_on_Demand_Holder/Singleton.java
+++ design-patterns/fundamental-patterns/creational/singleton/src/main/java/com/hard/_07_Initialization_on_Demand_Holder/Singleton.java
@@ -6,7 +6,7 @@ public class Singleton {
     }
 
     private static class SingletonHolder {
-        private final static Singleton instance = new Singleton();
+        private static final Singleton instance = new Singleton();
     }
 
     public static Singleton getInstance() {
