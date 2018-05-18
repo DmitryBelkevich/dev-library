e36253e62d84b5b45f36b0f0b073fc705a602307
hard <harleydav@mail.ru>
2018-05-18 13:33:13 +0300

    refactoring

--- development/game-development/src/main/java/com/hard/_00_my_template/_02/Main.java
+++ development/game-development/src/main/java/com/hard/_00_my_template/_02/Main.java
@@ -117,16 +117,16 @@ class Game {
 
         // check collision
 
-        if (x <= 0)
+        if (x < 0)
             x = 0;
 
-        if (y <= 0)
+        if (y < 0)
             y = 0;
 
-        if (x >= WIDTH - w)
+        if (x > WIDTH - w)
             x = WIDTH - w;
 
-        if (y >= HEIGHT - h)
+        if (y > HEIGHT - h)
             y = HEIGHT - h;
 
         // stop moving
