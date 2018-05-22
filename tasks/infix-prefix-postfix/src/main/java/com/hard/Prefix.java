5b38b3e2ea1a4c37840f473a17475e4d179c2ebd
hard <harleydav@mail.ru>
2018-05-22 16:08:22 +0300

    refactoring

--- tasks/infix-prefix-postfix/src/main/java/com/hard/Prefix.java
+++ tasks/infix-prefix-postfix/src/main/java/com/hard/Prefix.java
@@ -1,5 +1,7 @@
 package com.hard;
 
+import java.util.Stack;
+
 public class Prefix {
     private char separator;
 
@@ -8,6 +10,56 @@ public class Prefix {
     }
 
     public double evaluate(String expression) {
-        return 0;
+        Stack<Double> operands = new Stack<>();
+
+        for (int i = expression.length() - 1; i >= 0; i--) {
+            char currentChar = expression.charAt(i);
+
+            if (currentChar == separator)
+                continue;
+            else if (Character.isDigit(currentChar) || currentChar == '.') {
+                StringBuilder stringBuilder = new StringBuilder();
+
+                while (true) {
+                    currentChar = expression.charAt(i);
+
+                    if (!Character.isDigit(currentChar))
+                        if (currentChar != '.')
+                            if (currentChar == separator)
+                                break;
+
+                    stringBuilder.insert(0, currentChar);
+
+                    i--;
+                }
+
+                double operand = Double.valueOf(stringBuilder.toString());
+
+                operands.push(operand);
+            } else if (currentChar == '+' || currentChar == '-' || currentChar == '*' || currentChar == '/' || currentChar == '^') {
+                double operand1 = operands.pop();
+                double operand2 = operands.pop();
+
+                switch (currentChar) {
+                    case '+':
+                        operands.push(operand1 + operand2);
+                        break;
+                    case '-':
+                        operands.push(operand1 - operand2);
+                        break;
+                    case '*':
+                        operands.push(operand1 * operand2);
+                        break;
+                    case '/':
+                        operands.push(operand1 / operand2);
+                        break;
+                    case '^':
+                        operands.push(Math.pow(operand1, operand2));
+                        break;
+                }
+            }
+        }
+
+        return operands.pop();
     }
 }
--- tasks/infix-prefix-postfix/src/test/java/com/hard/PrefixTest.java
+++ tasks/infix-prefix-postfix/src/test/java/com/hard/PrefixTest.java
@@ -17,7 +17,7 @@ public class PrefixTest {
     public void should_evaluate_expression_2() {
         Prefix prefix = new Prefix();
 
-        String expression = "* 2.0 2.0 + 2.0";  // infix: 2+2*2
+        String expression = "+ * 2.0 2.0 2.0";  // infix: 2+2*2
         double result = prefix.evaluate(expression);
         Assert.assertEquals(6, result, 0);
     }
@@ -26,17 +26,8 @@ public class PrefixTest {
     public void should_evaluate_expression_3() {
         Prefix prefix = new Prefix();
 
-        String expression = "^ ^ 2.0 2.0 2.0";  // infix: 2^2^2
+        String expression = "^ 2.0 ^ 3.0 2.0";  // infix: 2^3^2
         double result = prefix.evaluate(expression);
-        Assert.assertEquals(6, result, 0);
-    }
-
-    @Test//del
-    public void should_evaluate_expression_4() {
-        Prefix prefix = new Prefix();
-
-        String expression = "- + * 2 3 + 5 4 9";  // infix: 2*3+5*4-9
-        double result = prefix.evaluate(expression);
-        Assert.assertEquals(17, result, 0);
+        Assert.assertEquals(512, result, 0);
     }
 }
