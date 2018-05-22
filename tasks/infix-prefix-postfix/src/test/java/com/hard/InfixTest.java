6d09f6a8dc0292bd52830c5d20cb101118db9daa
hard <harleydav@mail.ru>
2018-05-22 17:40:05 +0300

    refactoring

--- tasks/infix-prefix-postfix/src/test/java/com/hard/InfixTest.java
+++ tasks/infix-prefix-postfix/src/test/java/com/hard/InfixTest.java
@@ -7,24 +7,30 @@ public class InfixTest {
     public static class infix_to_postfix {
         @Test
         public void should_convert_infix_to_postfix_1() {
-            String infix = "(2.0+2.0)*2.0";
-            String postfix = Infix.toPostfix(infix);
+            Infix infix = new Infix();
+
+            String expression = "(2.0+2.0)*2.0";
+            String postfix = infix.toPostfix(expression);
 
             Assert.assertSame("2.0 2.0 + 2.0 *", postfix);
         }
 
         @Test
         public void should_convert_infix_to_postfix_2() {
-            String infix = "2+2*2";
-            String postfix = Infix.toPostfix(infix);
+            Infix infix = new Infix();
+
+            String expression = "2+2*2";
+            String postfix = infix.toPostfix(expression);
 
             Assert.assertSame("2.0 2.0 2.0 * +", postfix);
         }
 
         @Test
         public void should_convert_infix_to_postfix_3() {
-            String infix = "2^3^2";
-            String postfix = Infix.toPostfix(infix);
+            Infix infix = new Infix();
+
+            String expression = "2^3^2";
+            String postfix = infix.toPostfix(expression);
 
             Assert.assertSame("2 3 2 ^ ^", postfix);
         }
@@ -33,24 +39,30 @@ public class InfixTest {
     public static class infix_to_prefix {
         @Test
         public void should_convert_infix_to_prefix_1() {
-            String infix = "(2.0+2.0)*2.0";
-            String prefix = Infix.toPrefix(infix);
+            Infix infix = new Infix();
+
+            String expression = "(2.0+2.0)*2.0";
+            String prefix = infix.toPrefix(expression);
 
             Assert.assertSame("* + 2.0 2.0 2.0", prefix);
         }
 
         @Test
         public void should_convert_infix_to_prefix_2() {
-            String infix = "2.0+2.0*2.0";
-            String prefix = Infix.toPrefix(infix);
+            Infix infix = new Infix();
+
+            String expression = "2.0+2.0*2.0";
+            String prefix = infix.toPrefix(expression);
 
             Assert.assertSame("+ * 2.0 2.0 2.0", prefix);
         }
 
         @Test
         public void should_convert_infix_to_prefix_3() {
-            String infix = "2.0^3.0^2.0";
-            String prefix = Infix.toPrefix(infix);
+            Infix infix = new Infix();
+
+            String expression = "2.0^3.0^2.0";
+            String prefix = infix.toPrefix(expression);
 
             Assert.assertSame("^ 2.0 ^ 3.0 2.0", prefix);
         }
