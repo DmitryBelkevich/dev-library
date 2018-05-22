e22b7f9c983fb9eb8a12712f465a120866feae8f
hard <harleydav@mail.ru>
2018-05-22 11:43:03 +0300

    refactoring

---
+++ tasks/infix-prefix-postfix/src/main/java/com/hard/Main.java
@@ -0,0 +1,13 @@
+package com.hard;
+
+public class Main {
+    public static void main(String[] args) {
+        String expression = "2.1 2.2 + 2.3 *";
+        double result = evaluate(expression);
+        System.out.println(result);
+    }
+
+    private static double evaluate(String expression) {
+        return 0;
+    }
+}
