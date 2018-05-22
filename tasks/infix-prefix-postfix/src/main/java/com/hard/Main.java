7b7c7112d9cbfb52e9b25c09bc2171ea1b2f42d8
hard <harleydav@mail.ru>
2018-05-22 11:45:08 +0300

    is digit || .

--- tasks/infix-prefix-postfix/src/main/java/com/hard/Main.java
+++ tasks/infix-prefix-postfix/src/main/java/com/hard/Main.java
@@ -1,5 +1,7 @@
 package com.hard;
 
+import java.util.Stack;
+
 public class Main {
     public static void main(String[] args) {
         String expression = "2.1 2.2 + 2.3 *";
@@ -8,6 +10,37 @@ public class Main {
     }
 
     private static double evaluate(String expression) {
+        Stack<String> stack = new Stack<>();
+
+        char delimeter = ' ';
+
+        for (int i = 0; i < expression.length(); i++) {
+            char currentChar = expression.charAt(i);
+
+            if (currentChar == delimeter)
+                continue;
+            if (Character.isDigit(currentChar) || currentChar == '.') {
+                StringBuilder stringBuilder = new StringBuilder();
+                int j = i;
+                while (true) {
+                     if (!Character.isDigit(expression.charAt(j)))
+                         if (expression.charAt(j) != '.')
+                             if (expression.charAt(j) == delimeter)
+                                 break;
+
+                    stringBuilder.append(expression.charAt(j));
+
+                    j++;
+                }
+
+                i = j;
+
+                stack.push(stringBuilder.toString());
+            }
+        }
+
+        System.out.println(stack);
+
         return 0;
     }
 }
