57d029341453b2991dd2e8fa098c4bdccf1f9e66
hard <harleydav@mail.ru>
2018-05-22 13:10:50 +0300

    refactoring

--- tasks/infix-prefix-postfix/src/test/java/com/hard/PostfixTest.java
+++ tasks/infix-prefix-postfix/src/test/java/com/hard/PostfixTest.java
@@ -21,4 +21,13 @@ public class PostfixTest {
         double result = postfix.evaluate(expression);
         Assert.assertEquals(6, result, 0);
     }
+
+    @Test
+    public void should_evaluate_expression_3() {
+        Postfix postfix = new Postfix();
+
+        String expression = "2 3 2 ^ ^";  // infix: 2^3^2
+        double result = postfix.evaluate(expression);
+        Assert.assertEquals(512, result, 0);
+    }
 }
