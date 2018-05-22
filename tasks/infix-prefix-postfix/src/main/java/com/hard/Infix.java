540e93a627572afacfb3c3330b0ac58c788b8827
hard <harleydav@mail.ru>
2018-05-22 20:12:49 +0300

    refactoring

--- tasks/infix-prefix-postfix/src/main/java/com/hard/Infix.java
+++ tasks/infix-prefix-postfix/src/main/java/com/hard/Infix.java
@@ -1,11 +1,89 @@
 package com.hard;
 
+import java.util.Stack;
+
 public class Infix {
-    public static String toPostfix(String infix) {
+    private char separator;
+
+    public Infix() {
+        this.separator = ' ';
+    }
+
+    public char getSeparator() {
+        return separator;
+    }
+
+    public void setSeparator(char separator) {
+        this.separator = separator;
+    }
+
+    public String toPostfix(String infix) {
+        Stack<Character> operators = new Stack<>();
+
+        StringBuilder postfixBuilder = new StringBuilder();
+        boolean first = true;
+
+        for (int i = 0; i < infix.length(); i++) {
+            char currentChar = infix.charAt(i);
+
+            if (currentChar == separator)
+                continue;
+            else if (isOperand(currentChar)) {
+                StringBuilder digitBuilder = new StringBuilder();
+
+                while (true) {
+                    if (i >= infix.length())
+                        break;
+
+                    currentChar = infix.charAt(i);
+
+                    if (!isOperand(currentChar)) {
+                        i--;
+                        break;
+                    }
+
+                    digitBuilder.append(currentChar);
+
+                    i++;
+                }
+
+                if (first) {
+                    first = false;
+                    postfixBuilder.append(digitBuilder);
+                    continue;
+                }
+
+                postfixBuilder.append(separator);
+                postfixBuilder.append(digitBuilder);
+            } else if (isOperator(currentChar)) {
+                operators.push(currentChar);
+            }
+        }
+
+        while (!operators.empty()) {
+            postfixBuilder.append(separator);
+
+            char operator = operators.pop();
+            postfixBuilder.append(operator);
+        }
+
+        return postfixBuilder.toString();
+    }
+
+    public String toPrefix(String infix) {
         return null;
     }
 
-    public static String toPrefix(String infix) {
-        return null;
+    private boolean isOperand(char character) {
+        return Character.isDigit(character) ||
+                character == '.';
+    }
+
+    private boolean isOperator(char character) {
+        return character == '+' ||
+                character == '-' ||
+                character == '*' ||
+                character == '/' ||
+                character == '^';
     }
 }
