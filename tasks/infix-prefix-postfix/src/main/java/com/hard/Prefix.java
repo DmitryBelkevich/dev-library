189b38e2afae3e2ed6bbb144769ebb07ccdea103
hard <harleydav@mail.ru>
2018-05-22 15:24:47 +0300

    Prefix init

---
+++ tasks/infix-prefix-postfix/src/main/java/com/hard/Prefix.java
@@ -0,0 +1,13 @@
+package com.hard;
+
+public class Prefix {
+    private char separator;
+
+    public Prefix() {
+        this.separator = ' ';
+    }
+
+    public double evaluate(String expression) {
+        return 0;
+    }
+}
