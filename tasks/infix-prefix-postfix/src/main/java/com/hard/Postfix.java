ffc971323575813bd9a321eef44af9e3d727f013
hard <harleydav@mail.ru>
2018-05-22 12:03:05 +0300

    refactoring

---
+++ tasks/infix-prefix-postfix/src/main/java/com/hard/Postfix.java
@@ -0,0 +1,66 @@
+package com.hard;
+
+import java.util.Stack;
+
+public class Postfix {
+    public static void main(String[] args) {
+//        String expression = "2.0 2.0 + 2.0 *";//(2+2)*2
+//        double result = evaluate(expression);
+//        System.out.println(result);//8
+
+        String expression = "2.0 2.0 2.0 * +";//2+2*2
+        double result = evaluate(expression);
+        System.out.println(result);//6
+    }
+
+    private static double evaluate(String expression) {
+        Stack<Double> digits = new Stack<>();
+
+        char delimeter = ' ';
+
+        for (int i = 0; i < expression.length(); i++) {
+            char currentChar = expression.charAt(i);
+
+            if (currentChar == delimeter)
+                continue;
+            else if (Character.isDigit(currentChar) || currentChar == '.') {
+                StringBuilder stringBuilder = new StringBuilder();
+                int j = i;
+                while (true) {
+                     if (!Character.isDigit(expression.charAt(j)))
+                         if (expression.charAt(j) != '.')
+                             if (expression.charAt(j) == delimeter)
+                                 break;
+
+                    stringBuilder.append(expression.charAt(j));
+
+                    j++;
+                }
+
+                i = j;
+
+                digits.push(Double.valueOf(stringBuilder.toString()));
+            } else if (currentChar == '+' || currentChar == '-' || currentChar == '*' || currentChar == '/') {
+                double operand2 = digits.pop();
+                double operand1 = digits.pop();
+
+                switch (currentChar) {
+                    case '+':
+                        digits.push(operand1 + operand2);
+                        break;
+                    case '-':
+                        digits.push(operand1 - operand2);
+                        break;
+                    case '*':
+                        digits.push(operand1 * operand2);
+                        break;
+                    case '/':
+                        digits.push(operand1 / operand2);
+                        break;
+                }
+            }
+        }
+
+        return digits.pop();
+    }
+}
