2bfc3f200eeb58ec461cfbd56aa68b211e9df52e
hard <harleydav@mail.ru>
2018-05-24 04:58:49 +0300

    refactoring

--- development/game-development/games/hard-platformer-game/src/main/java/com/hard/entities/Console.java
+++ development/game-development/games/hard-platformer-game/src/main/java/com/hard/entities/Console.java
@@ -1,10 +1,13 @@
 package com.hard.entities;
 
+import com.hard.Game;
+
 import java.awt.*;
 
 public class Console {
     public void draw(Graphics graphics, Entity entity) {
-        int step = 20;
+        int stepX = 5;
+        int stepY = 20;
 
         double x = entity.getX();
         double y = entity.getY();
@@ -17,23 +20,23 @@ public class Console {
         graphics.setColor(new Color(0, 255, 0, 255));
 
 //        if (x != 0) graphics.setColor(new Color(225, 0, 0, 255));
-        graphics.drawString("x=" + x, 50, 1 * step);
+        graphics.drawString("x=" + x, 50, 1 * stepY);
 //        graphics.setColor(new Color(0, 255, 0, 255));
 
 //        if (y != 0) graphics.setColor(new Color(225, 0, 0, 255));
-        graphics.drawString("y=" + y, 50, 2 * step);
+        graphics.drawString("y=" + y, 50, 2 * stepY);
 //        graphics.setColor(new Color(0, 255, 0, 255));
 
         if (dx != 0) graphics.setColor(new Color(225, 0, 0, 255));
-        graphics.drawString("dx=" + dx, 50, 3 * step);
+        graphics.drawString("dx=" + dx, 50, 3 * stepY);
         graphics.setColor(new Color(0, 255, 0, 255));
 
         if (dy != 0) graphics.setColor(new Color(225, 0, 0, 255));
-        graphics.drawString("dy=" + dy, 50, 4 * step);
+        graphics.drawString("dy=" + dy, 50, 4 * stepY);
         graphics.setColor(new Color(0, 255, 0, 255));
 
         if (speed != 0) graphics.setColor(new Color(225, 0, 0, 255));
-        graphics.drawString("speed=" + speed, 50, 5 * step);
+        graphics.drawString("speed=" + speed, 50, 5 * stepY);
         graphics.setColor(new Color(0, 255, 0, 255));
 
         /**
@@ -46,19 +49,19 @@ public class Console {
         boolean sitting = entity.isSitting();
 
         if (left) graphics.setColor(new Color(225, 0, 0, 255));
-        graphics.drawString("left=" + left, 50, 7 * step);
+        graphics.drawString("left=" + left, 50, 7 * stepY);
         graphics.setColor(new Color(0, 255, 0, 255));
 
         if (right) graphics.setColor(new Color(225, 0, 0, 255));
-        graphics.drawString("right=" + right, 50, 8 * step);
+        graphics.drawString("right=" + right, 50, 8 * stepY);
         graphics.setColor(new Color(0, 255, 0, 255));
 
         if (jumping) graphics.setColor(new Color(225, 0, 0, 255));
-        graphics.drawString("jumping=" + jumping, 50, 9 * step);
+        graphics.drawString("jumping=" + jumping, 50, 9 * stepY);
         graphics.setColor(new Color(0, 255, 0, 255));
 
         if (sitting) graphics.setColor(new Color(225, 0, 0, 255));
-        graphics.drawString("sitting=" + sitting, 50, 10 * step);
+        graphics.drawString("sitting=" + sitting, 50, 10 * stepY);
         graphics.setColor(new Color(0, 255, 0, 255));
 
         /**
@@ -71,19 +74,34 @@ public class Console {
         boolean bottomCollision = entity.isBottomCollision();
 
         if (leftCollision) graphics.setColor(new Color(225, 0, 0, 255));
-        graphics.drawString("left collision=" + leftCollision, 50, 12 * step);
+        graphics.drawString("left collision=" + leftCollision, 50, 12 * stepY);
         graphics.setColor(new Color(0, 255, 0, 255));
 
         if (rightCollision) graphics.setColor(new Color(225, 0, 0, 255));
-        graphics.drawString("left collision=" + leftCollision, 50, 13 * step);
+        graphics.drawString("left collision=" + leftCollision, 50, 13 * stepY);
         graphics.setColor(new Color(0, 255, 0, 255));
 
         if (topCollision) graphics.setColor(new Color(225, 0, 0, 255));
-        graphics.drawString("top collision=" + topCollision, 50, 14 * step);
+        graphics.drawString("top collision=" + topCollision, 50, 14 * stepY);
         graphics.setColor(new Color(0, 255, 0, 255));
 
         if (bottomCollision) graphics.setColor(new Color(225, 0, 0, 255));
-        graphics.drawString("bottom collision=" + bottomCollision, 50, 15 * step);
+        graphics.drawString("bottom collision=" + bottomCollision, 50, 15 * stepY);
+        graphics.setColor(new Color(0, 255, 0, 255));
+
+        /**
+         * offset
+         */
+
+        double offsetX = Game.OFFSET_X;
+        double offsetY = Game.OFFSET_Y;
+
+        if (offsetX != 0) graphics.setColor(new Color(225, 0, 0, 255));
+        graphics.drawString("offset x=" + offsetX, 50 * stepX, 1 * stepY);
+        graphics.setColor(new Color(0, 255, 0, 255));
+
+        if (offsetY != 0) graphics.setColor(new Color(225, 0, 0, 255));
+        graphics.drawString("offset y=" + offsetY, 50 * stepX, 2 * stepY);
         graphics.setColor(new Color(0, 255, 0, 255));
     }
 }
