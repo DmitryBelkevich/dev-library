7d9acea39a9506f91d8be31837765f74f82722bc
hard <harleydav@mail.ru>
2018-05-25 22:12:44 +0300

    refactoring

--- development/game-development/games/hard-platformer-game/src/main/java/com/hard/Camera.java
+++
@@ -1,6 +0,0 @@
-package com.hard;
-
-public class Camera {
-    public static double x = 0;
-    public static double y = 0;
-}
---
+++ development/game-development/games/hard-platformer-game/src/main/java/com/hard/entities/Camera.java
@@ -0,0 +1,32 @@
+package com.hard.entities;
+
+import java.awt.*;
+
+public class Camera {
+    private double x;
+    private double y;
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public void update(double time) {
+
+    }
+
+    public void draw(Graphics graphics) {
+
+    }
+}
