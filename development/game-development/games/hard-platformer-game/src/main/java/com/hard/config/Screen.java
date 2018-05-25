18549c6499f4271ca6046b0c727270ff81b96f4c
hard <harleydav@mail.ru>
2018-05-25 04:18:41 +0300

    refactoring

---
+++ development/game-development/games/hard-platformer-game/src/main/java/com/hard/config/Screen.java
@@ -0,0 +1,9 @@
+package com.hard.config;
+
+import com.hard.Tile;
+
+public class Screen {
+    private static final double SCALE = 1.0;
+    public static final int WIDTH = (int) (Tile.H * 20 * SCALE);
+    public static final int HEIGHT = (int) (Tile.W * 15 * SCALE);
+}
