d2eba1866bfe6ba2aa30c06690719bf4915654f6
hard <harleydav@mail.ru>
2018-05-25 04:02:55 +0300

    refactoring

---
+++ development/game-development/games/hard-platformer-game/src/main/java/com/hard/maps/Maps.java
@@ -0,0 +1,28 @@
+package com.hard.maps;
+
+public class Maps {
+    public static class stage1 {
+        public static final String[] level1 = {
+                "BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB",
+                "B                                                B",
+                "B                                                B",
+                "B                                                B",
+                "B                                      00000     B",
+                "B                     BBBBB                      B",
+                "B                                                B",
+                "B                                                B",
+                "B                                                B",
+                "B         0000000             BBBBBBB            B",
+                "B                                B          BBBBBB",
+                "B                    BBBBBB      B               B",
+                "B                                B               B",
+                "B                                B               B",
+                "B                                B    BBBB       B",
+                "BBB             BBBBB            B               B",
+                "B                                BB           BBBB",
+                "B                                                B",
+                "B                         BB                     B",
+                "BBBBBBBBBBBBBBBB     BBBBBBBBBBBBBBBBBBBBBBBBBBBBB",
+        };
+    }
+}
