ff7f6b7f566bba8768a992ed6c42c243d34e7a49
hard <harleydav@mail.ru>
2018-05-29 02:15:26 +0300

    -

--- frameworks/lib-gdx/src/main/java/com/hard/DesktopLauncher.java
+++ frameworks/lib-gdx/src/main/java/com/hard/DesktopLauncher.java
@@ -6,6 +6,6 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 public class DesktopLauncher {
     public static void main(String[] args) {
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-        new LwjglApplication(new Game1(), config);
+//        new LwjglApplication(new Game1(), config);
     }
 }
