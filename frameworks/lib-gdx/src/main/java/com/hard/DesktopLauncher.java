15ea5c221466c25b2c03f06e9b42831b743f12ea
hard <harleydav@mail.ru>
2018-05-29 02:38:02 +0300

    refactoring

--- frameworks/lib-gdx/src/main/java/com/hard/DesktopLauncher.java
+++ frameworks/lib-gdx/src/main/java/com/hard/DesktopLauncher.java
@@ -5,7 +5,7 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
 public class DesktopLauncher {
     public static void main(String[] args) {
-        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+//        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 //        new LwjglApplication(new Game1(), config);
     }
 }
