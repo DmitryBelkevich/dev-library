c8b0ee2c7a25ce12088485490b38600f3b696426
hard <harleydav@mail.ru>
2018-05-30 11:12:39 +0300

    serialization-deserialization

--- java-core/io-streams/byte-streams/src/main/java/com/hard/byteArray/output/Main.java
+++ java-core/io-streams/byte-streams/src/main/java/com/hard/byteArray/output/Main.java
@@ -1,25 +1,35 @@
 package com.hard.byteArray.output;
 
-import java.io.*;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        OutputStream outputStream = new ByteArrayOutputStream();
-
         String str = "Hello World" + "\n"
-                + "Hello World"
-                ;
+                + "Hello World";
         byte[] bytes = str.getBytes();
 
-        try {
-            outputStream.write(bytes);
+        // write to byte[]
 
-            outputStream.flush();
-            outputStream.close();
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+
+        try {
+            byteArrayOutputStream.write(bytes);
         } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            try {
+                if (byteArrayOutputStream != null)
+                    byteArrayOutputStream.flush();
+
+                if (byteArrayOutputStream != null)
+                    byteArrayOutputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
 
-        System.out.println(outputStream);
+        System.out.println(byteArrayOutputStream.toString());
+        byte[] bytes2 = byteArrayOutputStream.toByteArray();
     }
 }
