9d42976111d995bc4a197aa5a0aaa4850a645fd7
hard <harleydav@mail.ru>
2018-05-30 10:20:28 +0300

    serialization-deserialization

--- java-core/io-streams/byte-streams/src/main/java/com/hard/file/output/Main.java
+++ java-core/io-streams/byte-streams/src/main/java/com/hard/file/output/Main.java
@@ -4,20 +4,21 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
+        String str = "Hello World" + "\n"
+                + "Hello World"
+                ;
+        byte[] bytes = str.getBytes();
+
+        // write to file
+
         File file = new File("c:/000.txt");
 
         OutputStream outputStream = null;
-
         try {
             outputStream = new FileOutputStream(file);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-
-        String str = "Hello World" + "\n"
-                + "Hello World"
-                ;
-        byte[] bytes = str.getBytes();
 
         try {
             outputStream.write(bytes);
