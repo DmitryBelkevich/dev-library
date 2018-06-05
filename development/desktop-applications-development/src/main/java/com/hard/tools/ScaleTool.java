9e52812fcb55decfb23b4bf1f53067255cd4dec4
hard <harleydav@mail.ru>
2018-06-06 02:13:10 +0300

    graphics editor

--- development/desktop-applications-development/src/main/java/com/hard/tools/ScaleTool.java
+++ development/desktop-applications-development/src/main/java/com/hard/tools/ScaleTool.java
@@ -1,5 +1,7 @@
 package com.hard.tools;
 
+import com.hard.models.Circle;
+import com.hard.models.Rectangle;
 import com.hard.models.Shape;
 
 import java.awt.event.MouseEvent;
@@ -26,21 +28,50 @@ public class ScaleTool extends Tool {
 
     @Override
     public void mousePressed(MouseEvent e) {
-
+        for (Shape shape : shapes) {
+            if (shape.intersected(e.getX(), e.getY())) {
+                shape.setSelected(true);
+            }
+        }
     }
 
     @Override
     public void mouseReleased(MouseEvent e) {
-
+        for (Shape shape : shapes) {
+            shape.setSelected(false);
+        }
     }
 
     @Override
     public void mouseMoved(MouseEvent e) {
-
+        for (Shape shape : shapes) {
+            if (!shape.isSelected()) {
+                if (shape.intersected(e.getX(), e.getY())) {
+                    shape.setOver(true);
+                } else {
+                    shape.setOver(false);
+                }
+            }
+        }
     }
 
     @Override
     public void mouseDragged(MouseEvent e) {
+        for (Shape shape : shapes) {
+            if (shape.isSelected()) {
+                if (shape instanceof Rectangle) {
+                    Rectangle rectangle = (Rectangle) shape;
 
+                    rectangle.setW(rectangle.getW() + 1);
+                    rectangle.setH(rectangle.getH() + 1);
+                }
+
+                if (shape instanceof Circle) {
+                    Circle circle = (Circle) shape;
+
+                    circle.setR(circle.getR() + 1);
+                }
+            }
+        }
     }
 }
