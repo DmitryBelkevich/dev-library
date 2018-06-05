6f1d4ed32459f29aa8508dba0dbae3504e0f8f98
hard <harleydav@mail.ru>
2018-06-05 23:56:33 +0300

    graphics editor

--- development/desktop-applications-development/src/main/java/com/hard/Main.java
+++ development/desktop-applications-development/src/main/java/com/hard/Main.java
@@ -2,7 +2,11 @@ package com.hard;
 
 import com.hard.controllers.Editor;
 import com.hard.models.Circle;
+import com.hard.models.Rectangle;
 import com.hard.models.Shape;
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 public class Main {
     public static void main(String[] args) {
@@ -14,8 +18,20 @@ public class Main {
         circle.setW(100);
         circle.setH(100);
 
+        Shape rectangle = new Rectangle();
+
+        rectangle.setX(200);
+        rectangle.setY(200);
+        rectangle.setW(100);
+        rectangle.setH(100);
+
+        Collection<Shape> shapes = new ArrayList<>();
+
+        shapes.add(circle);
+        shapes.add(rectangle);
+
         // controller
-        Editor editor = new Editor(circle);
+        Editor editor = new Editor(rectangle);
         editor.run();
     }
 }
