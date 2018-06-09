0d4f808b64b23f28c8605582cf555bd3e440b0e0
hard <harleydav@mail.ru>
2018-06-09 19:49:39 +0300

    chat

--- development/client-server-development/src/main/java/com/hard/my_chat/server/ClientThread.java
+++ development/client-server-development/src/main/java/com/hard/my_chat/server/ClientThread.java
@@ -1,19 +1,11 @@
-package com.hard.my_chat.client;
-
-import com.hard.my_chat.client.views.ConsoleView;
-import com.hard.my_chat.client.views.FrameView;
-import com.hard.my_chat.client.views.View;
+package com.hard.my_chat.server;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
-import java.util.ArrayList;
-import java.util.Collection;
 
-public class Client {
-    private Collection<View> views;
-
+public class ClientThread implements Runnable {
     private InputStream inputStream;
     private OutputStream outputStream;
 
@@ -21,34 +13,18 @@ public class Client {
 
     private boolean launched;
 
-    public Client() {
-        views = new ArrayList<>();
-
-        views.add(new ConsoleView(this));
-        views.add(new FrameView(this));
+    public ClientThread(Socket socket) {
+        this.socket = socket;
 
         launched = true;
     }
 
+    @Override
     public void run() {
-        init();
         initStreams();
 
         while (launched) {
 
-        }
-
-        stop();
-    }
-
-    public void init() {
-        for (View view : views)
-            view.run();
-
-        try {
-            socket = new Socket("localhost", 9999);
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
 
@@ -72,10 +48,5 @@ public class Client {
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    public void notifyAllViews(String str) {
-        for (View view : views)
-            view.getMessage(str);
     }
 }
--- development/client-server-development/src/main/java/com/hard/my_chat/server/Server.java
+++ development/client-server-development/src/main/java/com/hard/my_chat/server/Server.java
@@ -4,22 +4,71 @@ import com.hard.my_chat.server.views.ConsoleView;
 import com.hard.my_chat.server.views.FrameView;
 import com.hard.my_chat.server.views.View;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.LinkedList;
 
 public class Server {
     private Collection<View> views;
+
+    private ServerSocket serverSocket;
+    private Collection<ClientThread> clientThreads;
+
+    private boolean launched;
 
     public Server() {
         views = new ArrayList<>();
 
         views.add(new ConsoleView(this));
         views.add(new FrameView(this));
+
+        clientThreads = new LinkedList<>();
+
+        launched = true;
     }
 
     public void run() {
+        init();
+
+        while (launched) {
+            Socket socket = null;
+            try {
+                socket = serverSocket.accept();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            ClientThread clientThread = new ClientThread(socket);
+            clientThreads.add(clientThread);
+            new Thread(clientThread).start();
+        }
+
+        stop();
+    }
+
+    public void init() {
         for (View view : views)
             view.run();
+
+        try {
+            serverSocket = new ServerSocket(9999);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void stop() {
+        for (ClientThread clientThread : clientThreads)
+            clientThread.stop();
+
+        try {
+            serverSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void notifyAllViews(String str) {
