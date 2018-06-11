505b5a5d8034447ea78c6787e98a5518d166e9cb
hard <harleydav@mail.ru>
2018-06-11 19:29:42 +0300

    refactoring

--- frameworks/hibernate/hibernate-init/src/main/java/com/hard/Main.java
+++ frameworks/hibernate/hibernate-init/src/main/java/com/hard/Main.java
@@ -4,11 +4,28 @@ import com.hard.models.Category;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+
+import java.util.Properties;
 
 public class Main {
     public static void main(String[] args) {
+//        Properties properties = new Properties();
+//
+//        properties.put(Environment.DRIVER, "org.postgresql.Driver");
+//        properties.put(Environment.URL, "jdbc:postgresql://localhost:5432/database1");
+//        properties.put(Environment.USER, "postgres");
+//        properties.put(Environment.PASS, "1234");
+//
+//        properties.put(Environment.DIALECT, "org.hibernate.dialect.PostgreSQL95Dialect");
+//
+//        properties.put("hibernate.show_sql", "true");
+//        properties.put("hibernate.hbm2ddl.auto", "update");
+
         SessionFactory sessionFactory = new Configuration()
                 .configure()
+//                .addClass(Category.class)
+//                .setProperties(properties)
                 .buildSessionFactory();
 
         Session session = sessionFactory.getCurrentSession();
