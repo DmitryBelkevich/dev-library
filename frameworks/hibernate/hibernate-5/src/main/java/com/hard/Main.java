f6d99480f27918673bd2d6b80c453be213c81273
hard <harleydav@mail.ru>
2018-06-11 19:07:47 +0300

    refactoring

--- frameworks/hibernate/hibernate-5/src/main/java/com/hard/Main.java
+++ frameworks/hibernate/hibernate-5/src/main/java/com/hard/Main.java
@@ -1,20 +1,11 @@
 package com.hard;
 
+import com.hard.models.Category;
+import com.hard.utils.HibernateUtil;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-import org.hibernate.boot.Metadata;
-import org.hibernate.boot.MetadataBuilder;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.SessionFactoryBuilder;
-import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Environment;
 import org.hibernate.query.NativeQuery;
-
-import javax.persistence.*;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
@@ -55,90 +46,5 @@ public class Main {
         }
 
         HibernateUtil.shutdown();
-    }
-}
-
-class HibernateUtil {
-    private static StandardServiceRegistry serviceRegistry;
-    private static SessionFactory sessionFactory;
-
-    public static SessionFactory getSessionFactory() {
-        if (sessionFactory == null) {
-            try {
-                // Create serviceRegistry builder
-                StandardServiceRegistryBuilder registryBuilder = new StandardServiceRegistryBuilder();
-
-                // Hibernate settings equivalent to hibernate.cfg.xml's properties
-                Map<String, String> settings = new HashMap<>();
-
-                settings.put(Environment.DRIVER, "org.postgresql.Driver");
-                settings.put(Environment.URL, "jdbc:postgresql://localhost:5432/database1");
-                settings.put(Environment.USER, "postgres");
-                settings.put(Environment.PASS, "1234");
-
-                settings.put(Environment.DIALECT, "org.hibernate.dialect.PostgreSQL95Dialect");
-
-                settings.put("hibernate.show_sql", "true");
-                settings.put("hibernate.hbm2ddl.auto", "update");
-
-                // Apply settings
-                registryBuilder.applySettings(settings);
-
-                // Create serviceRegistry
-                serviceRegistry = registryBuilder.build();
-
-                // Create MetadataSources
-                MetadataSources metadataSources = new MetadataSources(serviceRegistry);
-                metadataSources.addAnnotatedClass(Category.class);
-
-                // Create Metadata
-                MetadataBuilder metadataBuilder = metadataSources.getMetadataBuilder();
-                Metadata metadata = metadataBuilder.build();
-
-                // Create SessionFactory
-                SessionFactoryBuilder sessionFactoryBuilder = metadata.getSessionFactoryBuilder();
-
-                sessionFactory = sessionFactoryBuilder.build();
-            } catch (Exception e) {
-                e.printStackTrace();
-                if (serviceRegistry != null) {
-                    StandardServiceRegistryBuilder.destroy(serviceRegistry);
-                }
-            }
-        }
-
-        return sessionFactory;
-    }
-
-    public static void shutdown() {
-        if (serviceRegistry != null) {
-            StandardServiceRegistryBuilder.destroy(serviceRegistry);
-        }
-    }
-}
-
-@Entity
-@Table(name = "categories")
-class Category {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
-
-    private String title;
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
     }
 }
---
+++ frameworks/hibernate/hibernate-5/src/main/java/com/hard/utils/HibernateUtil.java
@@ -0,0 +1,73 @@
+package com.hard.utils;
+
+import com.hard.models.Category;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataBuilder;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.SessionFactoryBuilder;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Environment;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HibernateUtil {
+    private static StandardServiceRegistry serviceRegistry;
+    private static SessionFactory sessionFactory;
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                // Create serviceRegistry builder
+                StandardServiceRegistryBuilder registryBuilder = new StandardServiceRegistryBuilder();
+
+                // Hibernate settings equivalent to hibernate.cfg.xml's properties
+                Map<String, String> settings = new HashMap<>();
+
+                settings.put(Environment.DRIVER, "org.postgresql.Driver");
+                settings.put(Environment.URL, "jdbc:postgresql://localhost:5432/database1");
+                settings.put(Environment.USER, "postgres");
+                settings.put(Environment.PASS, "1234");
+
+                settings.put(Environment.DIALECT, "org.hibernate.dialect.PostgreSQL95Dialect");
+
+                settings.put("hibernate.show_sql", "true");
+                settings.put("hibernate.hbm2ddl.auto", "update");
+
+                // Apply settings
+                registryBuilder.applySettings(settings);
+
+                // Create serviceRegistry
+                serviceRegistry = registryBuilder.build();
+
+                // Create MetadataSources
+                MetadataSources metadataSources = new MetadataSources(serviceRegistry);
+                metadataSources.addAnnotatedClass(Category.class);
+
+                // Create Metadata
+                MetadataBuilder metadataBuilder = metadataSources.getMetadataBuilder();
+                Metadata metadata = metadataBuilder.build();
+
+                // Create SessionFactory
+                SessionFactoryBuilder sessionFactoryBuilder = metadata.getSessionFactoryBuilder();
+
+                sessionFactory = sessionFactoryBuilder.build();
+            } catch (Exception e) {
+                e.printStackTrace();
+                if (serviceRegistry != null) {
+                    StandardServiceRegistryBuilder.destroy(serviceRegistry);
+                }
+            }
+        }
+
+        return sessionFactory;
+    }
+
+    public static void shutdown() {
+        if (serviceRegistry != null) {
+            StandardServiceRegistryBuilder.destroy(serviceRegistry);
+        }
+    }
+}
